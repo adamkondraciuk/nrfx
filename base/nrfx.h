931bfbd2dcbe318489c91ec99c0a7616898c8dcc
Głąbek, Andrzej <andrzej.glabek@nordicsemi.no>
2017-04-12 12:16:00 +0200

    Refactored "base". Added first drivers.

--- base/nrfx.h
+++ base/nrfx.h
@@ -7,13 +7,17 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+//#include <nrfx_config.h>
+// TODO - temporary, use <nrfx_config.h> instead
+#include <sdk_config.h>
+
 #include <nrf.h>
 #include <nrf_peripherals.h>
 
-//#include <nrfx_config.h>
 #include <nrfx_macros.h>
-#include <nrfx_assert.h>
+#include <nrfx_errors.h>
+#include <nrfx_glue.h>
+
 //#include <nrfx_delay.h>
-#include <nrfx_log.h>
 
 #endif // NRFX_H__
