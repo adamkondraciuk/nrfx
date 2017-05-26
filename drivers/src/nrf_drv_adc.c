4a050bd1a5fb8ad5aeb069e1bb83f435e7f2d4bc
Głąbek, Andrzej <andrzej.glabek@nordicsemi.no>
2017-05-26 13:57:59 +0200

    Added ADC HAL and driver.

---
+++ drivers/src/nrf_drv_adc.c
@@ -0,0 +1,289 @@
+/*$$$LICENCE_NORDIC_STANDARD<2015>$$$*/
+
+#include <nrfx.h>
+
+#if NRFX_MODULE_ENABLED(ADC)
+#include <nrf_drv_adc.h>
+#include <nrf_drv_common.h>
+
+#define NRFX_LOG_MODULE ADC
+#include <nrfx_log.h>
+
+#define EVT_TO_STR(event)   (event == NRF_ADC_EVENT_END ? "NRF_ADC_EVENT_END" : "UNKNOWN EVENT")
+
+
+typedef struct
+{
+    nrf_drv_adc_event_handler_t event_handler;
+    nrf_drv_adc_channel_t     * p_head;
+    nrf_drv_adc_channel_t     * p_current_conv;
+    nrf_adc_value_t           * p_buffer;
+    uint8_t                     size;
+    uint8_t                     idx;
+    nrf_drv_state_t             state;
+} adc_cb_t;
+
+static adc_cb_t m_cb;
+static const nrf_drv_adc_config_t m_default_config = NRF_DRV_ADC_DEFAULT_CONFIG;
+
+ret_code_t nrf_drv_adc_init(nrf_drv_adc_config_t const * p_config,
+                            nrf_drv_adc_event_handler_t  event_handler)
+{
+    ret_code_t err_code;
+    
+    if (m_cb.state != NRF_DRV_STATE_UNINITIALIZED)
+    {
+        err_code = NRFX_ERROR_INVALID_STATE;
+        NRFX_LOG_WARNING("Function: %s, error code: %s.\r\n",
+            (uint32_t)__func__, (uint32_t)NRFX_LOG_ERROR_STRING_GET(err_code));
+        return err_code;
+    }
+
+    nrf_adc_event_clear(NRF_ADC_EVENT_END);
+    if (event_handler)
+    {
+        if (!p_config)
+        {
+            p_config = (nrf_drv_adc_config_t *)&m_default_config;
+        }
+        nrf_drv_common_irq_enable(ADC_IRQn, p_config->interrupt_priority);
+    }
+    m_cb.event_handler = event_handler;
+    m_cb.state = NRF_DRV_STATE_INITIALIZED;
+
+    err_code = NRFX_SUCCESS;
+    NRFX_LOG_INFO("Function: %s, error code: %s.\r\n",
+        (uint32_t)__func__, (uint32_t)NRFX_LOG_ERROR_STRING_GET(err_code));
+    return err_code;
+}
+
+void nrf_drv_adc_uninit(void)
+{
+    m_cb.p_head = NULL;
+    nrf_drv_common_irq_disable(ADC_IRQn);
+    nrf_adc_int_disable(NRF_ADC_INT_END_MASK);
+    nrf_adc_task_trigger(NRF_ADC_TASK_STOP);
+
+    m_cb.state = NRF_DRV_STATE_UNINITIALIZED;
+}
+
+void nrf_drv_adc_channel_enable(nrf_drv_adc_channel_t * const p_channel)
+{
+    NRFX_ASSERT(!is_address_from_stack(p_channel));
+    NRFX_ASSERT(!nrf_drv_adc_is_busy());
+
+    p_channel->p_next = NULL;
+    if (m_cb.p_head == NULL)
+    {
+        m_cb.p_head = p_channel;
+    }
+    else
+    {
+        nrf_drv_adc_channel_t * p_curr_channel = m_cb.p_head;
+        while (p_curr_channel->p_next != NULL)
+        {
+            NRFX_ASSERT(p_channel != p_curr_channel);
+            p_curr_channel = p_curr_channel->p_next;
+        }
+        p_curr_channel->p_next = p_channel;
+    }
+
+    NRFX_LOG_INFO("Enabled.\r\n");
+}
+
+void nrf_drv_adc_channel_disable(nrf_drv_adc_channel_t * const p_channel)
+{
+    NRFX_ASSERT(m_cb.p_head);
+    NRFX_ASSERT(!nrf_drv_adc_is_busy());
+
+    nrf_drv_adc_channel_t * p_curr_channel = m_cb.p_head;
+    nrf_drv_adc_channel_t * p_prev_channel = NULL;
+    while (p_curr_channel != p_channel)
+    {
+        p_prev_channel = p_curr_channel;
+        p_curr_channel = p_curr_channel->p_next;
+        NRFX_ASSERT(p_curr_channel != NULL);
+    }
+    if (p_prev_channel)
+    {
+        p_prev_channel->p_next = p_curr_channel->p_next;
+    }
+    else
+    {
+        m_cb.p_head = p_curr_channel->p_next;
+    }
+
+    NRFX_LOG_INFO("Disabled.\r\n");
+}
+
+void nrf_drv_adc_sample(void)
+{
+    NRFX_ASSERT(m_cb.state != NRF_DRV_STATE_UNINITIALIZED);
+    NRFX_ASSERT(!nrf_adc_is_busy());
+    nrf_adc_start();
+}
+
+ret_code_t nrf_drv_adc_sample_convert(nrf_drv_adc_channel_t const * const p_channel,
+                                      nrf_adc_value_t * p_value)
+{
+    ret_code_t err_code;
+
+    NRFX_ASSERT(m_cb.state != NRF_DRV_STATE_UNINITIALIZED);
+    if (m_cb.state == NRF_DRV_STATE_POWERED_ON)
+    {
+        err_code = NRFX_ERROR_BUSY;
+        NRFX_LOG_WARNING("Function: %s, error code: %s.\r\n",
+            (uint32_t)__func__, (uint32_t)NRFX_LOG_ERROR_STRING_GET(err_code));
+        return err_code;
+    }
+    else
+    {
+        m_cb.state = NRF_DRV_STATE_POWERED_ON;
+
+        nrf_adc_config_set(p_channel->config.data);
+        nrf_adc_enable();
+        nrf_adc_int_disable(NRF_ADC_INT_END_MASK);
+        nrf_adc_start();
+        if (p_value)
+        {
+            while (!nrf_adc_event_check(NRF_ADC_EVENT_END)) {}
+            nrf_adc_event_clear(NRF_ADC_EVENT_END);
+            *p_value = (nrf_adc_value_t)nrf_adc_result_get();
+            nrf_adc_disable();
+
+            m_cb.state = NRF_DRV_STATE_INITIALIZED;
+        }
+        else
+        {
+            NRFX_ASSERT(m_cb.event_handler);
+            m_cb.p_buffer = NULL;
+            nrf_adc_int_enable(NRF_ADC_INT_END_MASK);
+        }
+        err_code = NRFX_SUCCESS;
+        NRFX_LOG_INFO("Function: %s, error code: %s.\r\n",
+            (uint32_t)__func__, (uint32_t)NRFX_LOG_ERROR_STRING_GET(err_code));
+        return err_code;
+    }
+}
+
+static bool adc_sample_process()
+{
+    nrf_adc_event_clear(NRF_ADC_EVENT_END);
+    nrf_adc_disable();
+    m_cb.p_buffer[m_cb.idx] = (nrf_adc_value_t)nrf_adc_result_get();
+    m_cb.idx++;
+    if (m_cb.idx < m_cb.size)
+    {
+        bool task_trigger = false;
+        if (m_cb.p_current_conv->p_next == NULL)
+        {
+            m_cb.p_current_conv = m_cb.p_head;
+        }
+        else
+        {
+            m_cb.p_current_conv = m_cb.p_current_conv->p_next;
+            task_trigger = true;
+        }
+        nrf_adc_config_set(m_cb.p_current_conv->config.data);
+        nrf_adc_enable();
+        if (task_trigger)
+        {
+            //nrf_adc_start();
+            nrf_adc_task_trigger(NRF_ADC_TASK_START);
+        }
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+ret_code_t nrf_drv_adc_buffer_convert(nrf_adc_value_t * buffer, uint16_t size)
+{
+    NRFX_ASSERT(m_cb.state != NRF_DRV_STATE_UNINITIALIZED);
+
+    ret_code_t err_code;
+
+    NRFX_LOG_INFO("Number of samples requested to convert: %d.\r\n", size);
+
+    if (m_cb.state == NRF_DRV_STATE_POWERED_ON)
+    {
+        err_code = NRFX_ERROR_BUSY;
+        NRFX_LOG_WARNING("Function: %s, error code: %s.\r\n",
+            (uint32_t)__func__, (uint32_t)NRFX_LOG_ERROR_STRING_GET(err_code));
+        return err_code;
+    }
+    else
+    {
+        m_cb.state          = NRF_DRV_STATE_POWERED_ON;
+        m_cb.p_current_conv = m_cb.p_head;
+        m_cb.size           = size;
+        m_cb.idx            = 0;
+        m_cb.p_buffer       = buffer;
+        nrf_adc_config_set(m_cb.p_current_conv->config.data);
+        nrf_adc_event_clear(NRF_ADC_EVENT_END);
+        nrf_adc_enable();
+        if (m_cb.event_handler)
+        {
+            nrf_adc_int_enable(NRF_ADC_INT_END_MASK);
+        }
+        else
+        {
+            while (1)
+            {
+                while (!nrf_adc_event_check(NRF_ADC_EVENT_END)){}
+
+                if (adc_sample_process())
+                {
+                    m_cb.state = NRF_DRV_STATE_INITIALIZED;
+                    break;
+                }
+            }
+        }
+        err_code = NRFX_SUCCESS;
+        NRFX_LOG_INFO("Function: %s, error code: %s.\r\n",
+            (uint32_t)__func__, (uint32_t)NRFX_LOG_ERROR_STRING_GET(err_code));
+        return err_code;
+    }
+}
+
+bool nrf_drv_adc_is_busy(void)
+{
+    NRFX_ASSERT(m_cb.state != NRF_DRV_STATE_UNINITIALIZED);
+    return (m_cb.state == NRF_DRV_STATE_POWERED_ON) ? true : false;
+}
+
+void ADC_IRQHandler(void)
+{
+    if (m_cb.p_buffer == NULL)
+    {
+        nrf_adc_event_clear(NRF_ADC_EVENT_END);
+        NRFX_LOG_DEBUG("Event: %s.\r\n",
+            (uint32_t)NRFX_LOG_ERROR_STRING_GET(NRF_ADC_EVENT_END));
+        nrf_adc_int_disable(NRF_ADC_INT_END_MASK);
+        nrf_adc_disable();
+        nrf_drv_adc_evt_t evt;
+        evt.type = NRF_DRV_ADC_EVT_SAMPLE;
+        evt.data.sample.sample = (nrf_adc_value_t)nrf_adc_result_get();
+        NRFX_LOG_DEBUG("ADC data:\r\n");
+        NRFX_LOG_HEXDUMP_DEBUG((uint8_t *)(&evt.data.sample.sample), sizeof(nrf_adc_value_t));
+        m_cb.state = NRF_DRV_STATE_INITIALIZED;
+        m_cb.event_handler(&evt);
+    }
+    else if (adc_sample_process())
+    {
+        NRFX_LOG_DEBUG("Event: %s.\r\n",
+            (uint32_t)NRFX_LOG_ERROR_STRING_GET(NRF_ADC_EVENT_END));
+        nrf_adc_int_disable(NRF_ADC_INT_END_MASK);
+        nrf_drv_adc_evt_t evt;
+        evt.type = NRF_DRV_ADC_EVT_DONE;
+        evt.data.done.p_buffer = m_cb.p_buffer;
+        evt.data.done.size     = m_cb.size;
+        m_cb.state = NRF_DRV_STATE_INITIALIZED;
+        NRFX_LOG_DEBUG("ADC data:\r\n");
+        NRFX_LOG_HEXDUMP_DEBUG((uint8_t *)m_cb.p_buffer, m_cb.size * sizeof(nrf_adc_value_t));
+        m_cb.event_handler(&evt);
+    }
+}
+#endif // NRFX_MODULE_ENABLED(ADC)
