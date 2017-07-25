9b941ee2237d9048ed71918956c380366688a3c4
kl-cruz <karol.lasonczyk@nordicsemi.no>
2017-07-25 10:41:57 +0200

    NRFFOSDK-9848: Refactor hal files
    Moved function from c file to header and change it to inline static to
    keep consistency across other hal files.

--- hal/nrf_saadc.h
+++ hal/nrf_saadc.h
@@ -554,8 +554,19 @@ __STATIC_INLINE nrf_saadc_oversample_t nrf_saadc_oversample_get(void)
  * @param[in] channel Channel number.
  * @param[in] config  Pointer to the channel configuration structure.
  */
-void nrf_saadc_channel_init(uint8_t channel, nrf_saadc_channel_config_t const * const config);
-
+__STATIC_INLINE void nrf_saadc_channel_init(uint8_t                                  channel,
+                                            nrf_saadc_channel_config_t const * const config)
+{
+    NRF_SAADC->CH[channel].CONFIG =
+            ((config->resistor_p   << SAADC_CH_CONFIG_RESP_Pos)   & SAADC_CH_CONFIG_RESP_Msk)
+            | ((config->resistor_n << SAADC_CH_CONFIG_RESN_Pos)   & SAADC_CH_CONFIG_RESN_Msk)
+            | ((config->gain       << SAADC_CH_CONFIG_GAIN_Pos)   & SAADC_CH_CONFIG_GAIN_Msk)
+            | ((config->reference  << SAADC_CH_CONFIG_REFSEL_Pos) & SAADC_CH_CONFIG_REFSEL_Msk)
+            | ((config->acq_time   << SAADC_CH_CONFIG_TACQ_Pos)   & SAADC_CH_CONFIG_TACQ_Msk)
+            | ((config->mode       << SAADC_CH_CONFIG_MODE_Pos)   & SAADC_CH_CONFIG_MODE_Msk)
+            | ((config->burst      << SAADC_CH_CONFIG_BURST_Pos)  & SAADC_CH_CONFIG_BURST_Msk);
+    nrf_saadc_channel_input_set(channel, config->pin_p, config->pin_n);
+}
 /**
  *@}
  **/
