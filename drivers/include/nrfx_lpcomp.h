2b172c06d9a61552fc23ff7f5acea642ea4b57bc
Głąbek, Andrzej <andrzej.glabek@nordicsemi.no>
2017-09-15 15:40:40 +0200

    NRFFOSDK-10149: Move LPCOMP driver to nrfx namespace

---
+++ drivers/include/nrfx_lpcomp.h
@@ -0,0 +1,117 @@
+/*$$$LICENCE_NORDIC_STANDARD<2014>$$$*/
+
+#ifndef NRFX_LPCOMP_H__
+#define NRFX_LPCOMP_H__
+
+#include <nrfx.h>
+#include <hal/nrf_lpcomp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @addtogroup nrf_lpcomp LPCOMP HAL and driver
+ * @ingroup nrf_drivers
+ * @brief Low Power Comparator (LPCOMP) APIs.
+ *
+ * The LPCOMP HAL provides basic APIs for accessing the registers of Low Power Comparator.
+ * The LPCOMP driver provides APIs on a higher level.
+ *
+ * @defgroup nrfx_lpcomp LPCOMP driver
+ * @{
+ * @ingroup nrf_lpcomp
+ * @brief Low Power Comparator (LPCOMP) driver.
+ */
+
+/**
+ * @brief LPCOMP event handler function type.
+ * @param[in] event  LPCOMP event.
+ */
+typedef void (* nrfx_lpcomp_event_handler_t)(nrf_lpcomp_event_t event);
+
+/** @brief LPCOMP configuration. */
+typedef struct
+{
+    nrf_lpcomp_config_t hal;                /**< LPCOMP HAL configuration. */
+    nrf_lpcomp_input_t  input;              /**< Input to be monitored. */
+    uint8_t             interrupt_priority; /**< LPCOMP interrupt priority. */
+} nrfx_lpcomp_config_t;
+
+/** @brief LPCOMP driver default configuration including the LPCOMP HAL configuration. */
+#ifdef NRF52_SERIES
+#define NRFX_LPCOMP_DEFAULT_CONFIG                                      \
+    {                                                                   \
+        .hal    = { (nrf_lpcomp_ref_t)NRFX_LPCOMP_CONFIG_REFERENCE ,    \
+                    (nrf_lpcomp_detect_t)NRFX_LPCOMP_CONFIG_DETECTION,  \
+                    (nrf_lpcomp_hysteresis_t)NRFX_LPCOMP_CONFIG_HYST }, \
+        .input  = (nrf_lpcomp_input_t)NRFX_LPCOMP_CONFIG_INPUT,         \
+        .interrupt_priority = NRFX_LPCOMP_CONFIG_IRQ_PRIORITY           \
+    }
+#else
+#define NRFX_LPCOMP_DEFAULT_CONFIG                                       \
+    {                                                                    \
+        .hal    = { (nrf_lpcomp_ref_t)NRFX_LPCOMP_CONFIG_REFERENCE ,     \
+                    (nrf_lpcomp_detect_t)NRFX_LPCOMP_CONFIG_DETECTION }, \
+        .input  = (nrf_lpcomp_input_t)NRFX_LPCOMP_CONFIG_INPUT,          \
+        .interrupt_priority = NRFX_LPCOMP_CONFIG_IRQ_PRIORITY            \
+    }
+#endif
+
+/**
+ * @brief Function for initializing the LPCOMP driver.
+ *
+ * This function initializes the LPCOMP driver, but does not enable the peripheral or any interrupts.
+ * To start the driver, call the function nrfx_lpcomp_enable() after initialization.
+ *
+ * If no configuration structure is provided, the driver is initialized with the default settings.
+ *
+ * @param[in] p_config      Initial configuration. If NULL, the default configuration is used.
+ * @param[in] event_handler Handler function.
+ *
+ * @retval NRFX_ERROR_INVALID_PARAM If the configuration is invalid.
+ * @retval NRFX_ERROR_INVALID_STATE If the driver has already been initialized.
+ */
+ret_code_t nrfx_lpcomp_init(nrfx_lpcomp_config_t const * p_config,
+                            nrfx_lpcomp_event_handler_t  event_handler);
+
+/**
+ *  @brief Function for uninitializing the LCOMP driver.
+ *
+ *  This function uninitializes the LPCOMP driver. The LPCOMP peripheral and
+ *  its interrupts are disabled, and local variables are cleaned. After this call, you must
+ *  initialize the driver again by calling nrfx_lpcomp_init() if you want to use it.
+ *
+ *  @sa nrfx_lpcomp_disable()
+ *  @sa nrfx_lpcomp_init()
+ */
+void  nrfx_lpcomp_uninit(void);
+
+/**@brief Function for enabling the LPCOMP peripheral and interrupts.
+ *
+ * Before calling this function, the driver must be initialized. This function
+ * enables the LPCOMP peripheral and its interrupts.
+ *
+ * @sa nrfx_lpcomp_disable()
+ */
+void nrfx_lpcomp_enable(void);
+
+/**@brief Function for disabling the LPCOMP peripheral.
+ *
+ * Before calling this function, the driver must be initialized. This function disables the LPCOMP
+ * peripheral and its interrupts.
+ *
+ * @sa nrfx_lpcomp_enable()
+ */
+void nrfx_lpcomp_disable(void);
+
+
+void nrfx_lpcomp_irq_handler(void);
+
+/** @} **/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // NRFX_LPCOMP_H__
--- drivers/src/nrfx_lpcomp.c
+++ drivers/src/nrfx_lpcomp.c
@@ -2,9 +2,9 @@
 
 #include <nrfx.h>
 
-#if NRFX_CHECK(LPCOMP_ENABLED)
+#if NRFX_CHECK(NRFX_LPCOMP_ENABLED)
 
-#include <nrf_drv_lpcomp.h>
+#include <nrfx_lpcomp.h>
 #include "prs/nrfx_prs.h"
 
 #define NRFX_LOG_MODULE LPCOMP
@@ -18,30 +18,30 @@
                                        "UNKNOWN EVENT"))))
 
 
-static lpcomp_events_handler_t m_lpcomp_events_handler = NULL;
-static nrfx_drv_state_t        m_state = NRFX_DRV_STATE_UNINITIALIZED;
+static nrfx_lpcomp_event_handler_t  m_lpcomp_event_handler = NULL;
+static nrfx_drv_state_t             m_state = NRFX_DRV_STATE_UNINITIALIZED;
 
 static void lpcomp_execute_handler(nrf_lpcomp_event_t event, uint32_t event_mask)
 {
-    if ( nrf_lpcomp_event_check(event) && nrf_lpcomp_int_enable_check(event_mask) )
+    if (nrf_lpcomp_event_check(event) && nrf_lpcomp_int_enable_check(event_mask))
     {
         nrf_lpcomp_event_clear(event);
         NRFX_LOG_DEBUG("Event: %s.", (uint32_t)EVT_TO_STR(event));
 
-        m_lpcomp_events_handler(event);
+        m_lpcomp_event_handler(event);
     }
 }
 
 void nrfx_lpcomp_irq_handler(void)
 {
     lpcomp_execute_handler(NRF_LPCOMP_EVENT_READY, LPCOMP_INTENSET_READY_Msk);
-    lpcomp_execute_handler(NRF_LPCOMP_EVENT_DOWN, LPCOMP_INTENSET_DOWN_Msk);
-    lpcomp_execute_handler(NRF_LPCOMP_EVENT_UP, LPCOMP_INTENSET_UP_Msk);
+    lpcomp_execute_handler(NRF_LPCOMP_EVENT_DOWN,  LPCOMP_INTENSET_DOWN_Msk);
+    lpcomp_execute_handler(NRF_LPCOMP_EVENT_UP,    LPCOMP_INTENSET_UP_Msk);
     lpcomp_execute_handler(NRF_LPCOMP_EVENT_CROSS, LPCOMP_INTENSET_CROSS_Msk);
 }
 
-ret_code_t nrf_drv_lpcomp_init(const nrf_drv_lpcomp_config_t * p_config,
-                               lpcomp_events_handler_t   events_handler)
+ret_code_t nrfx_lpcomp_init(nrfx_lpcomp_config_t const * p_config,
+                            nrfx_lpcomp_event_handler_t  event_handler)
 {
     NRFX_ASSERT(p_config);
     ret_code_t err_code;
@@ -49,6 +49,19 @@ ret_code_t nrf_drv_lpcomp_init(const nrf_drv_lpcomp_config_t * p_config,
     if (m_state != NRFX_DRV_STATE_UNINITIALIZED)
     { // LPCOMP driver is already initialized
         err_code = NRFX_ERROR_INVALID_STATE;
+        NRFX_LOG_WARNING("Function: %s, error code: %s.",
+                         (uint32_t)__func__,
+                         (uint32_t)NRFX_LOG_ERROR_STRING_GET(err_code));
+        return err_code;
+    }
+
+    if (event_handler)
+    {
+        m_lpcomp_event_handler = event_handler;
+    }
+    else
+    {
+        err_code = NRFX_ERROR_INVALID_PARAM;
         NRFX_LOG_WARNING("Function: %s, error code: %s.",
                          (uint32_t)__func__,
                          (uint32_t)NRFX_LOG_ERROR_STRING_GET(err_code));
@@ -66,20 +79,7 @@ ret_code_t nrf_drv_lpcomp_init(const nrf_drv_lpcomp_config_t * p_config,
     }
 #endif
 
-    nrf_lpcomp_configure(&(p_config->hal) );
-
-    if (events_handler)
-    {
-        m_lpcomp_events_handler = events_handler;
-    }
-    else
-    {
-        err_code = NRFX_ERROR_INVALID_PARAM;
-        NRFX_LOG_WARNING("Function: %s, error code: %s.",
-                         (uint32_t)__func__,
-                         (uint32_t)NRFX_LOG_ERROR_STRING_GET(err_code));
-        return err_code;
-    }
+    nrf_lpcomp_configure(&(p_config->hal));
 
     nrf_lpcomp_input_select(p_config->input);
 
@@ -114,20 +114,20 @@ ret_code_t nrf_drv_lpcomp_init(const nrf_drv_lpcomp_config_t * p_config,
     return err_code;
 }
 
-void nrf_drv_lpcomp_uninit(void)
+void nrfx_lpcomp_uninit(void)
 {
     NRFX_ASSERT(m_state != NRFX_DRV_STATE_UNINITIALIZED);
     NRFX_IRQ_DISABLE(LPCOMP_IRQn);
-    nrf_drv_lpcomp_disable();
+    nrfx_lpcomp_disable();
 #if NRFX_CHECK(NRFX_PRS_ENABLED)
     nrfx_prs_release(NRF_LPCOMP);
 #endif
     m_state = NRFX_DRV_STATE_UNINITIALIZED;
-    m_lpcomp_events_handler = NULL;
+    m_lpcomp_event_handler = NULL;
     NRFX_LOG_INFO("Uninitialized.");
 }
 
-void nrf_drv_lpcomp_enable(void)
+void nrfx_lpcomp_enable(void)
 {
     NRFX_ASSERT(m_state == NRFX_DRV_STATE_INITIALIZED);
     nrf_lpcomp_enable();
@@ -136,7 +136,7 @@ void nrf_drv_lpcomp_enable(void)
     NRFX_LOG_INFO("Enabled.");
 }
 
-void nrf_drv_lpcomp_disable(void)
+void nrfx_lpcomp_disable(void)
 {
     NRFX_ASSERT(m_state == NRFX_DRV_STATE_POWERED_ON);
     nrf_lpcomp_disable();
@@ -145,9 +145,4 @@ void nrf_drv_lpcomp_disable(void)
     NRFX_LOG_INFO("Disabled.");
 }
 
-void nrf_drv_lpcomp_event_handler_register(lpcomp_events_handler_t lpcomp_events_handler)
-{
-    m_lpcomp_events_handler = lpcomp_events_handler;
-}
-
-#endif // NRFX_CHECK(LPCOMP_ENABLED)
+#endif // NRFX_CHECK(NRFX_LPCOMP_ENABLED)
