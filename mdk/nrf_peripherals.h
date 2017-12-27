acd4a0e70abe6c5106d37c2709861b01cdc96b7d
Karol Lasończyk <karol.lasonczyk@nordicsemi.no>
2017-12-27 10:45:17 +0100

    NRFFOSDK-11070: Add missing few nRF51s support
    Commit adds nRF51801 and nRF51824 support in nrf_peripherals.h file.

--- mdk/nrf_peripherals.h
+++ mdk/nrf_peripherals.h
@@ -7,10 +7,14 @@
 
 #if defined(NRF51422)
     #include "nrf51422_peripherals.h"
+#elif defined(NRF51801)
+    #include "nrf51801_peripherals.h"
 #elif defined(NRF51802)
     #include "nrf51802_peripherals.h"
 #elif defined(NRF51822)
     #include "nrf51822_peripherals.h"
+#elif defined(NRF51824)
+    #include "nrf51824_peripherals.h"
 #elif defined(NRF52810_XXAA)
     #include "nrf52810_peripherals.h"
 #elif defined(NRF52832_XXAA) || defined(NRF52832_XXAB)
