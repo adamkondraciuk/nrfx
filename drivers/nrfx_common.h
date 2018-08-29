99d0860a8d90c6b773fc563c81231996b45fdc32
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2018-08-29 15:08:26 +0200

    NRFFOSDK-12315: Add NRFX_ARRAY_SIZE macro used in array bounds asserts

--- drivers/nrfx_common.h
+++ drivers/nrfx_common.h
@@ -88,6 +88,15 @@ extern "C" {
  */
 #define NRFX_ROUNDED_DIV(a, b)  (((a) + ((b) / 2)) / (b))
 
+/**
+ * @brief Macro for getting the number of elements in an array.
+ *
+ * @param array  Name of the array.
+ *
+ * @return Array element count.
+ */
+#define NRFX_ARRAY_SIZE(array) (sizeof(array) / sizeof((array)[0]))
+
 /**@brief Macro for checking if given lengths of EasyDMA transfers do not exceed
  *        the limit of the specified peripheral.
  *
