2c07793c2d9d6db27ce1837bcfc7ee9b343cc511
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2018-09-04 12:14:22 +0200

    NRFFOSDK-12045: Add default config to QDEC driver

--- drivers/include/nrfx_qdec.h
+++ drivers/include/nrfx_qdec.h
@@ -32,20 +32,34 @@ typedef struct
     uint8_t              interrupt_priority; /**< QDEC interrupt priority. */
 } nrfx_qdec_config_t;
 
-/**@brief QDEC default configuration. */
-#define NRFX_QDEC_DEFAULT_CONFIG                                            \
-{                                                                           \
-    .reportper          = (nrf_qdec_reportper_t)NRFX_QDEC_CONFIG_REPORTPER, \
-    .sampleper          = (nrf_qdec_sampleper_t)NRFX_QDEC_CONFIG_SAMPLEPER, \
-    .psela              = NRFX_QDEC_CONFIG_PIO_A,                           \
-    .pselb              = NRFX_QDEC_CONFIG_PIO_B,                           \
-    .pselled            = NRFX_QDEC_CONFIG_PIO_LED,                         \
-    .ledpre             = NRFX_QDEC_CONFIG_LEDPRE,                          \
-    .ledpol             = (nrf_qdec_ledpol_t)NRFX_QDEC_CONFIG_LEDPOL,       \
-    .dbfen              = NRFX_QDEC_CONFIG_DBFEN,                           \
-    .sample_inten       = NRFX_QDEC_CONFIG_SAMPLE_INTEN,                    \
-    .interrupt_priority = NRFX_QDEC_CONFIG_IRQ_PRIORITY,                    \
-}
+/**
+ * @brief QDEC driver default configuration.
+ *
+ * This configuration sets up QDEC with the following options:
+ * - report period: 10 samples
+ * - sampling period: 16384 us
+ * - LED enabled for 500 us before sampling
+ * - LED polarity: active high
+ * - debouncing filter disabled
+ * - sample ready interrupt disabled
+ *
+ * @param[in] _pin_a   Pin for A encoder channel input.
+ * @param[in] _pin_b   Pin for B encoder channel input.
+ * @param[in] _pin_led Pin for LED output.
+ */
+#define NRFX_QDEC_DEFAULT_CONFIG(_pin_a, _pin_b, _pin_led)   \
+    {                                                        \
+        .reportper          = NRF_QDEC_REPORTPER_10,         \
+        .sampleper          = NRF_QDEC_SAMPLEPER_16384us,    \
+        .psela              = _pin_a,                        \
+        .pselb              = _pin_b,                        \
+        .pselled            = _pin_led,                      \
+        .ledpre             = 500,                           \
+        .ledpol             = NRF_QDEC_LEPOL_ACTIVE_HIGH,    \
+        .dbfen              = NRF_QDEC_DBFEN_DISABLE,        \
+        .sample_inten       = false,                         \
+        .interrupt_priority = NRFX_QDEC_CONFIG_IRQ_PRIORITY  \
+    }
 
 /** @brief QDEC sample event data. */
 typedef struct
