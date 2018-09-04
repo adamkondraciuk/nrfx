6074378c65cc0adacaa89ac2f21b12aec3b57d28
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2018-09-04 12:21:06 +0200

    NRFFOSDK-12045: Add default config to TWIM driver

--- drivers/include/nrfx_twim.h
+++ drivers/include/nrfx_twim.h
@@ -60,14 +60,23 @@ typedef struct
     bool                 hold_bus_uninit;    ///< Hold pull up state on GPIO pins after uninit.
 } nrfx_twim_config_t;
 
-/** @brief TWI master driver instance default configuration. */
-#define NRFX_TWIM_DEFAULT_CONFIG                                                    \
-{                                                                                   \
-    .scl                = 31,                                                       \
-    .sda                = 31,                                                       \
-    .frequency          = (nrf_twim_frequency_t)NRFX_TWIM_DEFAULT_CONFIG_FREQUENCY, \
-    .interrupt_priority = NRFX_TWIM_DEFAULT_CONFIG_IRQ_PRIORITY,                    \
-    .hold_bus_uninit    = NRFX_TWIM_DEFAULT_CONFIG_HOLD_BUS_UNINIT,                 \
+/**
+ * @brief TWIM driver default configuration.
+ *
+ * This configuration sets up TWIM with the following options:
+ * - clock frequency: 100 kHz
+ * - disable bus holding after uninit
+ *
+ * @param[in] _pin_scl SCL pin.
+ * @param[in] _pin_sda SDA pin.
+ */
+#define NRFX_TWIM_DEFAULT_CONFIG(_pin_scl, _pin_sda)              \
+{                                                                 \
+    .scl                = _pin_scl,                               \
+    .sda                = _pin_sda,                               \
+    .frequency          = NRF_TWIM_FREQ_100K,                     \
+    .interrupt_priority = NRFX_TWIM_DEFAULT_CONFIG_IRQ_PRIORITY,  \
+    .hold_bus_uninit    = false,                                  \
 }
 
 /** @brief Flag indicating that TX buffer address will be incremented after the transfer. */
