8e0ff9bab244f4bc8a65175322679cf74faef8fd
Kastelik <nikodem.kastelik@nordicsemi.no>
2018-12-18 08:14:28 +0100

    NRFFOSDK-12425: Align nrfx_twi & nrf_twi to nRF52811 TWI peripheral structure
    TWI peripheral structure in nRF52811 differs from the one found in nRF51.
    Access to individual pins has to be done via substructure.

--- drivers/src/nrfx_twi.c
+++ drivers/src/nrfx_twi.c
@@ -180,8 +180,8 @@ void nrfx_twi_uninit(nrfx_twi_t const * p_instance)
 
     if (!p_cb->hold_bus_uninit)
     {
-        nrf_gpio_cfg_default(p_instance->p_twi->PSELSCL);
-        nrf_gpio_cfg_default(p_instance->p_twi->PSELSDA);
+        nrf_gpio_cfg_default(nrf_twi_scl_pin_get(p_instance->p_twi));
+        nrf_gpio_cfg_default(nrf_twi_sda_pin_get(p_instance->p_twi));
     }
 
     p_cb->state = NRFX_DRV_STATE_UNINITIALIZED;
