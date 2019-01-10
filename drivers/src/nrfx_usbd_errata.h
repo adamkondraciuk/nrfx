3910ca4706cae6e6074da6b3537509ba83a7ae24
Marcin Szymczyk <Marcin.Szymczyk@nordicsemi.no>
2019-01-10 10:40:14 +0100

    NRFFOSDK-12546: Backport USBD changes from SDK
    This commit introduces following fixes in USBD driver:
    - USBD IRQ not cleared on stop
    - Report corrupted at remote wakeup
    - Align USBD errata

--- drivers/src/nrfx_usbd_errata.h
+++ drivers/src/nrfx_usbd_errata.h
@@ -21,34 +21,34 @@ static inline bool nrfx_usbd_errata_type_52840(void)
 
 static inline bool nrfx_usbd_errata_type_52840_eng_a(void)
 {
-    return (nrfx_usbd_errata_type_52840() && (*(uint32_t *)0x10000134UL == 0x0UL));
+    return nrfx_usbd_errata_type_52840();
 }
 
 static inline bool nrfx_usbd_errata_type_52840_eng_b(void)
 {
-    return (nrfx_usbd_errata_type_52840() && (*(uint32_t *)0x10000134UL == 0x1UL));
+    return (nrfx_usbd_errata_type_52840() && (*(uint32_t *)0x10000134UL >= 0x1UL));
 }
 
 static inline bool nrfx_usbd_errata_type_52840_eng_c(void)
 {
-    return (nrfx_usbd_errata_type_52840() && (*(uint32_t *)0x10000134UL == 0x2UL));
+    return (nrfx_usbd_errata_type_52840() && (*(uint32_t *)0x10000134UL >= 0x2UL));
 }
 
 static inline bool nrfx_usbd_errata_type_52840_eng_d(void)
 {
-    return (nrfx_usbd_errata_type_52840() && (*(uint32_t *)0x10000134UL == 0x3UL));
+    return (nrfx_usbd_errata_type_52840() && (*(uint32_t *)0x10000134UL >= 0x3UL));
 }
 
 /* Errata: USBD: EPDATA event is not always generated. */
 static inline bool nrfx_usbd_errata_104(void)
 {
-    return (NRFX_USBD_ERRATA_ENABLE && nrfx_usbd_errata_type_52840_eng_a());
+    return (NRFX_USBD_ERRATA_ENABLE && (!nrfx_usbd_errata_type_52840_eng_b()));
 }
 
 /* Errata: During setup read/write transfer USBD acknowledges setup stage without SETUP task. */
 static inline bool nrfx_usbd_errata_154(void)
 {
-    return (NRFX_USBD_ERRATA_ENABLE && nrfx_usbd_errata_type_52840_eng_a());
+    return (NRFX_USBD_ERRATA_ENABLE && (!nrfx_usbd_errata_type_52840_eng_b()));
 }
 
 /* Errata: ISO double buffering not functional. */
@@ -66,11 +66,7 @@ static inline bool nrfx_usbd_errata_171(void)
 /* Errata: USB cannot be enabled. */
 static inline bool nrfx_usbd_errata_187(void)
 {
-    return (NRFX_USBD_ERRATA_ENABLE && 
-            (nrfx_usbd_errata_type_52840_eng_b() ||
-             nrfx_usbd_errata_type_52840_eng_c() ||
-             nrfx_usbd_errata_type_52840_eng_d())
-           );
+    return (NRFX_USBD_ERRATA_ENABLE && nrfx_usbd_errata_type_52840_eng_b());
 }
 
 /* Errata: USBD cannot receive tasks during DMA. */
@@ -82,7 +78,7 @@ static inline bool nrfx_usbd_errata_199(void)
 /* Errata: SIZE.EPOUT not writable. */
 static inline bool nrfx_usbd_errata_200(void)
 {
-    return (NRFX_USBD_ERRATA_ENABLE && nrfx_usbd_errata_type_52840_eng_a());
+    return (NRFX_USBD_ERRATA_ENABLE && (!nrfx_usbd_errata_type_52840_eng_b()));
 }
 
 #endif // NRFX_USBD_ERRATA_H__
