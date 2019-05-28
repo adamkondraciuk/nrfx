6fae06886b7ea5a31589f74416f26b54ff606e81
Karol Lasończyk <karol.lasonczyk@nordicsemi.no>
2019-05-28 15:00:56 +0200

    NCSDK-2093: Expand nrfx_qspi functions to check buffer address alignment

--- drivers/src/nrfx_qspi.c
+++ drivers/src/nrfx_qspi.c
@@ -321,16 +321,14 @@ nrfx_err_t nrfx_qspi_write(void const * p_tx_buffer,
     NRFX_ASSERT(m_cb.state != NRFX_DRV_STATE_UNINITIALIZED);
     NRFX_ASSERT(p_tx_buffer != NULL);
     NRFX_ASSERT(nrfx_is_in_ram(p_tx_buffer));
-    NRFX_ASSERT(nrfx_is_word_aligned(p_tx_buffer));
 
-    if (!nrfx_is_in_ram(p_tx_buffer))
+    if (!nrfx_is_in_ram(p_tx_buffer) || !nrfx_is_word_aligned(p_tx_buffer))
     {
         return NRFX_ERROR_INVALID_ADDR;
     }
 
     nrf_qspi_write_buffer_set(NRF_QSPI, p_tx_buffer, tx_buffer_length, dst_address);
     return qspi_task_perform(NRF_QSPI_TASK_WRITESTART);
-
 }
 
 nrfx_err_t nrfx_qspi_read(void *   p_rx_buffer,
@@ -340,9 +338,8 @@ nrfx_err_t nrfx_qspi_read(void *   p_rx_buffer,
     NRFX_ASSERT(m_cb.state != NRFX_DRV_STATE_UNINITIALIZED);
     NRFX_ASSERT(p_rx_buffer != NULL);
     NRFX_ASSERT(nrfx_is_in_ram(p_rx_buffer));
-    NRFX_ASSERT(nrfx_is_word_aligned(p_rx_buffer));
 
-    if (!nrfx_is_in_ram(p_rx_buffer))
+    if (!nrfx_is_in_ram(p_rx_buffer) || !nrfx_is_word_aligned(p_rx_buffer))
     {
         return NRFX_ERROR_INVALID_ADDR;
     }
@@ -355,6 +352,12 @@ nrfx_err_t nrfx_qspi_erase(nrf_qspi_erase_len_t length,
                            uint32_t             start_address)
 {
     NRFX_ASSERT(m_cb.state != NRFX_DRV_STATE_UNINITIALIZED);
+
+    if (!nrfx_is_word_aligned((void const *)start_address))
+    {
+        return NRFX_ERROR_INVALID_ADDR;
+    }
+
     nrf_qspi_erase_ptr_set(NRF_QSPI, start_address, length);
     return qspi_task_perform(NRF_QSPI_TASK_ERASESTART);
 }
