aefde280edb015c7c3ba4ce6668e9d9c953096b2
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2019-07-02 09:12:34 +0200

    NCSDK-2490: Fix support for external LF clock source for nrf52811
    Definition was missing nrf52811. Changed it to rely on register fields
    existance rather than soc name.

--- hal/nrf_clock.h
+++ hal/nrf_clock.h
@@ -19,9 +19,7 @@ extern "C" {
  * (HFCLK) settings.
  */
 
-#if defined(NRF52810_XXAA) || \
-    defined(NRF52832_XXAA) || defined(NRF52832_XXAB) || \
-    defined(NRF52840_XXAA)
+#if defined(CLOCK_LFCLKSRC_BYPASS_Msk) && defined(CLOCK_LFCLKSRC_EXTERNAL_Msk)
 // Enable support for external LFCLK sources. Read more in the Product Specification.
 #define NRF_CLOCK_USE_EXTERNAL_LFCLK_SOURCES
 #endif
