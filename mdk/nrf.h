d81208b58a9fb75957015ae8df8a7442a012c64f
Karol Lasończyk <karol.lasonczyk@nordicsemi.no>
2019-07-16 09:27:49 +0200

    NCSDK-2463: Update MDK to version 8.25.1

--- mdk/nrf.h
+++ mdk/nrf.h
@@ -43,9 +43,9 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define NRF_H
 
 /* MDK version */
-#define MDK_MAJOR_VERSION   8
-#define MDK_MINOR_VERSION   25
-#define MDK_MICRO_VERSION   0
+#define MDK_MAJOR_VERSION   8 
+#define MDK_MINOR_VERSION   25 
+#define MDK_MICRO_VERSION   1 
 
 /* Redefine "old" too-generic name NRF52 to NRF52832_XXAA to keep backwards compatibility. */
 #if defined (NRF52)
@@ -63,7 +63,7 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /* Define NRF91_SERIES for common use in nRF91 series devices. */
 #if defined (NRF9160_XXAA)
-    #ifndef NRF91_SERIES
+    #ifndef NRF91_SERIES    
         #define NRF91_SERIES
     #endif
 #endif
@@ -97,10 +97,10 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
         #include "nrf52810_name_change.h"
     #elif defined (NRF52811_XXAA)
         #include "nrf52811.h"
-        #include "nrf52811_bitfields.h"
-        #include "nrf51_to_nrf52810.h"
-        #include "nrf52_to_nrf52810.h"
-        #include "nrf52810_to_nrf52811.h"
+        #include "nrf52811_bitfields.h"  
+        #include "nrf51_to_nrf52.h"
+        #include "nrf52_to_nrf52810.h"   
+        #include "nrf52810_to_nrf52811.h"     
     #elif defined (NRF52832_XXAA) || defined (NRF52832_XXAB)
         #include "nrf52.h"
         #include "nrf52_bitfields.h"
@@ -116,12 +116,12 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
         #include "nrf52840_bitfields.h"
         #include "nrf51_to_nrf52840.h"
         #include "nrf52_to_nrf52840.h"
-
+    
     #elif defined (NRF9160_XXAA)
         #include "nrf9160.h"
         #include "nrf9160_bitfields.h"
         #include "nrf9160_name_change.h"
-
+        
     #else
         #error "Device must be defined. See nrf.h."
     #endif /* NRF51, NRF52805_XXAA, NRF52810_XXAA, NRF52811_XXAA, NRF52832_XXAA, NRF52832_XXAB, NRF52833_XXAA, NRF52840_XXAA, NRF9160_XXAA */
