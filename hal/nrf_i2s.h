6e026d5290589dcc596b14170d60b3af4515908f
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2019-10-17 14:03:26 +0200

    NCSDK-3103: Change API in nrf_i2s
    Interrupt getter returns bitmask instead of bool.

--- hal/nrf_i2s.h
+++ hal/nrf_i2s.h
@@ -204,16 +204,14 @@ NRF_STATIC_INLINE void nrf_i2s_int_enable(NRF_I2S_Type * p_reg, uint32_t mask);
 NRF_STATIC_INLINE void nrf_i2s_int_disable(NRF_I2S_Type * p_reg, uint32_t mask);
 
 /**
- * @brief Function for retrieving the state of a given interrupt.
+ * @brief Function for checking if the specified interrupts are enabled.
  *
- * @param[in] p_reg   Pointer to the structure of registers of the peripheral.
- * @param[in] i2s_int Interrupt to be checked.
+ * @param[in] p_reg Pointer to the structure of registers of the peripheral.
+ * @param[in] mask  Mask of interrupts to be checked.
  *
- * @retval true  The interrupt is enabled.
- * @retval false The interrupt is not enabled.
+ * @return Mask of enabled interrupts.
  */
-NRF_STATIC_INLINE bool nrf_i2s_int_enable_check(NRF_I2S_Type const * p_reg,
-                                                nrf_i2s_int_mask_t   i2s_int);
+NRF_STATIC_INLINE uint32_t nrf_i2s_int_enable_check(NRF_I2S_Type const * p_reg, uint32_t mask);
 
 /**
  * @brief Function for enabling the I2S peripheral.
@@ -428,10 +426,9 @@ NRF_STATIC_INLINE void nrf_i2s_int_disable(NRF_I2S_Type * p_reg, uint32_t mask)
     p_reg->INTENCLR = mask;
 }
 
-NRF_STATIC_INLINE bool nrf_i2s_int_enable_check(NRF_I2S_Type const * p_reg,
-                                                nrf_i2s_int_mask_t   i2s_int)
+NRF_STATIC_INLINE uint32_t nrf_i2s_int_enable_check(NRF_I2S_Type const * p_reg, uint32_t mask)
 {
-    return (bool)(p_reg->INTENSET & i2s_int);
+    return p_reg->INTENSET & mask;
 }
 
 NRF_STATIC_INLINE void nrf_i2s_enable(NRF_I2S_Type * p_reg)
