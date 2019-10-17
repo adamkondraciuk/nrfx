e4ab1ea40d170c5348c6cf6bb955fa3cdcd1f207
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2019-10-17 14:18:17 +0200

    NCSDK-3103: Change API in nrf_timer
    Interrupt getter returns bitmask instead of bool.

--- hal/nrf_timer.h
+++ hal/nrf_timer.h
@@ -307,16 +307,14 @@ NRF_STATIC_INLINE void nrf_timer_int_disable(NRF_TIMER_Type * p_reg,
                                              uint32_t         mask);
 
 /**
- * @brief Function for retrieving the state of a given interrupt.
+ * @brief Function for checking if the specified interrupts are enabled.
  *
- * @param[in] p_reg     Pointer to the structure of registers of the peripheral.
- * @param[in] timer_int Interrupt to be checked.
+ * @param[in] p_reg Pointer to the structure of registers of the peripheral.
+ * @param[in] mask  Mask of interrupts to be checked.
  *
- * @retval true  The interrupt is enabled.
- * @retval false The interrupt is not enabled.
+ * @return Mask of enabled interrupts.
  */
-NRF_STATIC_INLINE bool nrf_timer_int_enable_check(NRF_TIMER_Type const * p_reg,
-                                                  uint32_t               timer_int);
+NRF_STATIC_INLINE uint32_t nrf_timer_int_enable_check(NRF_TIMER_Type const * p_reg, uint32_t mask);
 
 #if defined(DPPI_PRESENT) || defined(__NRFX_DOXYGEN__)
 /**
@@ -572,10 +570,9 @@ NRF_STATIC_INLINE void nrf_timer_int_disable(NRF_TIMER_Type * p_reg,
     p_reg->INTENCLR = mask;
 }
 
-NRF_STATIC_INLINE bool nrf_timer_int_enable_check(NRF_TIMER_Type const * p_reg,
-                                                  uint32_t               timer_int)
+NRF_STATIC_INLINE uint32_t nrf_timer_int_enable_check(NRF_TIMER_Type const * p_reg, uint32_t mask)
 {
-    return (bool)(p_reg->INTENSET & timer_int);
+    return p_reg->INTENSET & mask;
 }
 
 #if defined(DPPI_PRESENT)
