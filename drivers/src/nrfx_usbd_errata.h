df6499c5e63441cd7254ce485803ba5f0d13dd6d
Karol Lasończyk <karol.lasonczyk@nordicsemi.no>
2019-11-27 11:12:42 +0100

    NCSDK-3643: Change the way of handling erratas in nrfx_usbd driver

--- drivers/src/nrfx_usbd_errata.h
+++ drivers/src/nrfx_usbd_errata.h
@@ -4,6 +4,8 @@
 #define NRFX_USBD_ERRATA_H__
 
 #include <stdbool.h>
+#include <nrfx.h>
+#include <nrf_erratas.h>
 
 #ifndef NRFX_USBD_ERRATA_ENABLE
 /**
@@ -14,106 +16,74 @@
 #define NRFX_USBD_ERRATA_ENABLE 1
 #endif
 
-static inline bool nrfx_usbd_errata_type_52840(void)
-{
-    return (*(uint32_t const *)0x10000130UL == 0x8UL);
-}
-
-static inline bool nrfx_usbd_errata_type_52840_eng_a_or_later(void)
-{
-    return nrfx_usbd_errata_type_52840();
-}
-
-static inline bool nrfx_usbd_errata_type_52840_eng_b_or_later(void)
-{
-    return (nrfx_usbd_errata_type_52840() && (*(uint32_t const *)0x10000134UL >= 0x1UL));
-}
-
-static inline bool nrfx_usbd_errata_type_52840_eng_c_or_later(void)
-{
-    return (nrfx_usbd_errata_type_52840() && (*(uint32_t const *)0x10000134UL >= 0x2UL));
-}
-
-static inline bool nrfx_usbd_errata_type_52840_eng_d_or_later(void)
-{
-    return (nrfx_usbd_errata_type_52840() && (*(uint32_t const *)0x10000134UL >= 0x3UL));
-}
-
-static inline bool nrfx_usbd_errata_type_52833(void)
-{
-    return (*(uint32_t const *)0x10000130UL == 0x0DUL);
-}
-
-static inline bool nrfx_usbd_errata_type_52833_eng_a_or_later(void)
-{
-    return nrfx_usbd_errata_type_52833();
-}
-
-/* Errata: USBD: EPDATA event is not always generated.
- *
- * Applies to nRF52840 Engineering A.
- **/
+/* Errata: USBD: EPDATA event is not always generated. **/
 static inline bool nrfx_usbd_errata_104(void)
 {
-    return (NRFX_USBD_ERRATA_ENABLE && (nrfx_usbd_errata_type_52840()
-                                        && !nrfx_usbd_errata_type_52840_eng_b_or_later()));
+#if defined(NRF52_SERIES) && NRFX_USBD_ERRATA_ENABLE
+    return errata_104();
+#else
+    return false;
+#endif
 }
 
-/* Errata: During setup read/write transfer USBD acknowledges setup stage without SETUP task.
- *
- * Applies to nRF52840 Engineering A.
- **/
+/* Errata: During setup read/write transfer USBD acknowledges setup stage without SETUP task. **/
 static inline bool nrfx_usbd_errata_154(void)
 {
-    return (NRFX_USBD_ERRATA_ENABLE && (nrfx_usbd_errata_type_52840()
-                                        && !nrfx_usbd_errata_type_52840_eng_b_or_later()));
+#if defined(NRF52_SERIES) && NRFX_USBD_ERRATA_ENABLE
+    return errata_154();
+#else
+    return false;
+#endif
 }
 
-/* Errata: ISO double buffering not functional.
- *
- * Applies to nRF52840.
- **/
+/* Errata: ISO double buffering not functional. **/
 static inline bool nrfx_usbd_errata_166(void)
 {
-    return (NRFX_USBD_ERRATA_ENABLE && nrfx_usbd_errata_type_52840());
+#if defined(NRF52_SERIES) && NRFX_USBD_ERRATA_ENABLE
+    return errata_166();
+#else
+    return false;
+#endif
 }
 
-/* Errata: USBD might not reach its active state.
- *
- * Applies to nRF52840.
- **/
+/* Errata: USBD might not reach its active state. **/
 static inline bool nrfx_usbd_errata_171(void)
 {
-    return (NRFX_USBD_ERRATA_ENABLE && nrfx_usbd_errata_type_52840());
+#if defined(NRF52_SERIES) && NRFX_USBD_ERRATA_ENABLE
+    return errata_171();
+#else
+    return false;
+#endif
 }
 
-/* Errata: USB cannot be enabled.
- *
- * Applies to nRF52840 Engineering B or later and nRF52833.
- **/
+/* Errata: USB cannot be enabled. **/
 static inline bool nrfx_usbd_errata_187(void)
 {
-    return (NRFX_USBD_ERRATA_ENABLE && (nrfx_usbd_errata_type_52840_eng_b_or_later()
-                                        || nrfx_usbd_errata_type_52833()));
+#if defined(NRF52_SERIES) && NRFX_USBD_ERRATA_ENABLE
+    return errata_187();
+#else
+    return false;
+#endif
 }
 
-/* Errata: USBD cannot receive tasks during DMA.
- *
- * Applies to nRF52840.
- **/
+/* Errata: USBD cannot receive tasks during DMA. **/
 static inline bool nrfx_usbd_errata_199(void)
 {
-    return (NRFX_USBD_ERRATA_ENABLE && nrfx_usbd_errata_type_52840());
+#if defined(NRF52_SERIES) && NRFX_USBD_ERRATA_ENABLE
+    return errata_199();
+#else
+    return false;
+#endif
 }
 
-/* Errata: SIZE.EPOUT not writable.
- *
- * Applies to nRF52840 Engineering A.
- **/
+/* Errata: SIZE.EPOUT not writable. **/
 static inline bool nrfx_usbd_errata_200(void)
 {
-    return (NRFX_USBD_ERRATA_ENABLE && (nrfx_usbd_errata_type_52840()
-                                        && !nrfx_usbd_errata_type_52840_eng_b_or_later()));
+#if defined(NRF52_SERIES) && NRFX_USBD_ERRATA_ENABLE
+    return errata_200();
+#else
+    return false;
+#endif
 }
 
 #endif // NRFX_USBD_ERRATA_H__
