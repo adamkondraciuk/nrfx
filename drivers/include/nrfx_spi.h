dac0305d2e1c1784899b255b92a268ac396bd811
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2019-12-02 16:20:58 +0100

    NCSDK-NONE: Add missing miso_pull field to default SPI configuration
    To prevent compilation errors when used in C++ code.

--- drivers/include/nrfx_spi.h
+++ drivers/include/nrfx_spi.h
@@ -87,6 +87,7 @@ typedef struct
  * - clock frequency 4 MHz
  * - mode 0 enabled (SCK active high, sample on leading edge of clock)
  * - MSB shifted out first
+ * - MISO pull-up disabled
  *
  * @param[in] _pin_sck  SCK pin.
  * @param[in] _pin_mosi MOSI pin.
@@ -104,6 +105,7 @@ typedef struct
     .frequency    = NRF_SPI_FREQ_4M,                                        \
     .mode         = NRF_SPI_MODE_0,                                         \
     .bit_order    = NRF_SPI_BIT_ORDER_MSB_FIRST,                            \
+    .miso_pull    = NRF_GPIO_PIN_NOPULL,                                    \
 }
 
 /** @brief Single transfer descriptor structure. */
