8f56204996d423c22fd6e48e19c5726d562c3b0e
Karol Lasończyk <karol.lasonczyk@nordicsemi.no>
2020-01-03 13:20:42 +0100

    NCSDK-NONE: Update default configuration in LPCOMP driver
    Patch updates default configuration to set up correctly all the
    structures inside main structure. It fixes not-initialized hysteresis
    for nRF5340.

--- drivers/include/nrfx_lpcomp.h
+++ drivers/include/nrfx_lpcomp.h
@@ -41,7 +41,7 @@ typedef struct
  *
  * @param[in] _input Comparator input pin.
  */
-#ifdef NRF52_SERIES
+#if defined(LPCOMP_FEATURE_HYST_PRESENT)
 #define NRFX_LPCOMP_DEFAULT_CONFIG(_input)                         \
 {                                                                  \
     .hal    = {  NRF_LPCOMP_REF_SUPPLY_4_8,                        \
