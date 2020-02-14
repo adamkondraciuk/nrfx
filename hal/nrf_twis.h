355237b5d3018e78a7f241bb8559d28fe46887c9
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2020-02-14 08:35:05 +0100

    NCSDK-2272: Add pin getters to nrf_twis

--- hal/nrf_twis.h
+++ hal/nrf_twis.h
@@ -293,6 +293,24 @@ NRF_STATIC_INLINE void nrf_twis_disable(NRF_TWIS_Type * p_reg);
 NRF_STATIC_INLINE void nrf_twis_pins_set(NRF_TWIS_Type * p_reg, uint32_t scl, uint32_t sda);
 
 /**
+ * @brief Function for retrieving the SCL pin number.
+ *
+ * @param[in] p_reg Pointer to the structure of registers of the peripheral.
+ *
+ * @return SCL pin number.
+ */
+NRF_STATIC_INLINE uint32_t nrf_twis_scl_pin_get(NRF_TWIS_Type const * p_reg);
+
+/**
+ * @brief Function for retrieving the SDA pin number.
+ *
+ * @param[in] p_reg Pointer to the structure of registers of the peripheral.
+ *
+ * @return SDA pin number.
+ */
+NRF_STATIC_INLINE uint32_t nrf_twis_sda_pin_get(NRF_TWIS_Type const * p_reg);
+
+/**
  * @brief Function for setting the receive buffer.
  *
  * @param[in] p_reg  Pointer to the structure of registers of the peripheral.
@@ -639,6 +657,16 @@ NRF_STATIC_INLINE void nrf_twis_pins_set(NRF_TWIS_Type * p_reg, uint32_t scl, ui
 {
     p_reg->PSEL.SCL = scl;
     p_reg->PSEL.SDA = sda;
+}
+
+NRF_STATIC_INLINE uint32_t nrf_twis_scl_pin_get(NRF_TWIS_Type const * p_reg)
+{
+    return p_reg->PSEL.SCL;
+}
+
+NRF_STATIC_INLINE uint32_t nrf_twis_sda_pin_get(NRF_TWIS_Type const * p_reg)
+{
+    return p_reg->PSEL.SDA;
 }
 
 NRF_STATIC_INLINE void nrf_twis_rx_buffer_set(NRF_TWIS_Type * p_reg,
