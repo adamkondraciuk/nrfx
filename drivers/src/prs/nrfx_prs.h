33815ea2ac638079916339fad2932ce68fa6d7d4
Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>
2020-04-10 13:27:19 +0200

    NRFX-68: Add nrfx_prs for Haltium
    Signed-off-by: Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>

--- drivers/src/prs/nrfx_prs.h
+++ drivers/src/prs/nrfx_prs.h
@@ -78,6 +78,15 @@ extern "C" {
     #define NRFX_PRS_BOX_2_ADDR     NRF_UARTE2
     // UARTE3, SPIM3, SPIS3, TWIM3, TWIS3
     #define NRFX_PRS_BOX_3_ADDR     NRF_UARTE3
+#elif defined(HALTIUM_XXAA)
+    // SPIM0, SPIS0, TWIM0, TWIS0, UARTE0
+    #define NRFX_PRS_BOX_0_ADDR     NRF_UARTE0
+    // SPIM1, SPIS1, TWIM1, TWIS1, UARTE1
+    #define NRFX_PRS_BOX_1_ADDR     NRF_UARTE1
+    // SPIM2, SPIS2, TWIM2, TWIS2, UARTE2
+    #define NRFX_PRS_BOX_2_ADDR     NRF_UARTE2
+    // SPIM3, SPIS3, TWIM3, TWIS3, UARTE3
+    #define NRFX_PRS_BOX_3_ADDR     NRF_UARTE3
 #else
     #error "Unknown device."
 #endif
