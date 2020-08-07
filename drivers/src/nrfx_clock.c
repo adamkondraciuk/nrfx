339a35f2bf58bd7f8fe24287f1fe48270c2e4b1f
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2020-08-07 12:46:03 +0200

    NRFX-329: Add optional two-stage start of LFXO in nrfx_clock

--- drivers/src/nrfx_clock.c
+++ drivers/src/nrfx_clock.c
@@ -56,6 +56,30 @@ extern bool nrfx_power_irq_enabled;
     #define USE_WORKAROUND_FOR_ANOMALY_201 1
 #endif
 
+#if defined(CLOCK_LFCLKSRC_SRC_Xtal)
+    #define LF_SRC_LFXO CLOCK_LFCLKSRC_SRC_Xtal
+#else
+    #define LF_SRC_LFXO CLOCK_LFCLKSRC_SRC_LFXO
+#endif
+
+#if defined(NRF_CLOCK_USE_EXTERNAL_LFCLK_SOURCES)
+    #define LF_SRC_XTAL_LOW  (CLOCK_LFCLKSRC_SRC_Xtal | \
+                             (CLOCK_LFCLKSRC_EXTERNAL_Enabled << CLOCK_LFCLKSRC_EXTERNAL_Pos))
+    #define LF_SRC_XTAL_FULL (CLOCK_LFCLKSRC_SRC_Xtal | \
+                             (CLOCK_LFCLKSRC_BYPASS_Enabled   << CLOCK_LFCLKSRC_BYPASS_Pos) | \
+                             (CLOCK_LFCLKSRC_EXTERNAL_Enabled << CLOCK_LFCLKSRC_EXTERNAL_Pos))
+#else
+    #define LF_SRC_XTAL_LOW  LF_SRC_LFXO
+    #define LF_SRC_XTAL_FULL LF_SRC_LFXO
+#endif
+
+#if NRFX_CHECK(NRFX_CLOCK_CONFIG_LFXO_TWO_STAGE_ENABLED) && \
+    NRFX_CLOCK_CONFIG_LF_SRC != LF_SRC_LFXO && \
+    NRFX_CLOCK_CONFIG_LF_SRC != LF_SRC_XTAL_LOW && \
+    NRFX_CLOCK_CONFIG_LF_SRC != LF_SRC_XTAL_FULL
+    #error "Two-stage LFXO start procedure enabled but LFCLK source is not set to LFXO!"
+#endif
+
 #if NRFX_CHECK(NRFX_CLOCK_CONFIG_LF_CAL_ENABLED)
 typedef enum
 {
@@ -150,7 +174,9 @@ void nrfx_clock_enable(void)
 {
     NRFX_ASSERT(m_clock_cb.module_initialized);
     nrfx_power_clock_irq_init();
+#if !NRFX_CHECK(NRFX_CLOCK_CONFIG_LFXO_TWO_STAGE_ENABLED)
     nrf_clock_lf_src_set(NRF_CLOCK, (nrf_clock_lfclk_t)NRFX_CLOCK_CONFIG_LF_SRC);
+#endif
 #if NRF_CLOCK_HAS_HFCLKSRC
     nrf_clock_hf_src_set(NRF_CLOCK, NRF_CLOCK_HFCLK_HIGH_ACCURACY);
 #endif
@@ -210,6 +236,25 @@ void nrfx_clock_start(nrf_clock_domain_t domain)
     switch (domain)
     {
         case NRF_CLOCK_DOMAIN_LFCLK:
+#if NRFX_CHECK(NRFX_CLOCK_CONFIG_LFXO_TWO_STAGE_ENABLED)
+            {
+                nrf_clock_lfclk_t lfclksrc;
+                if (nrf_clock_is_running(NRF_CLOCK, NRF_CLOCK_DOMAIN_LFCLK, &lfclksrc) &&
+                    lfclksrc == NRFX_CLOCK_CONFIG_LF_SRC)
+                {
+                    // If the two-stage LFXO procedure has finished already
+                    // use the configured LF clock source.
+                    nrf_clock_lf_src_set(NRF_CLOCK, (nrf_clock_lfclk_t)NRFX_CLOCK_CONFIG_LF_SRC);
+                }
+                else
+                {
+                    // If the two-stage LFXO procedure hasn't started yet
+                    // or the RC stage is in progress,
+                    // use the RC oscillator as LF clock source.
+                    nrf_clock_lf_src_set(NRF_CLOCK, NRF_CLOCK_LFCLK_RC);
+                }
+            }
+#endif // NRFX_CHECK(NRFX_CLOCK_CONFIG_LFXO_TWO_STAGE_ENABLED)
             nrf_clock_event_clear(NRF_CLOCK, NRF_CLOCK_EVENT_LFCLKSTARTED);
             nrf_clock_int_enable(NRF_CLOCK, NRF_CLOCK_INT_LF_STARTED_MASK);
 #if NRFX_CHECK(USE_WORKAROUND_FOR_ANOMALY_132)
@@ -457,9 +502,23 @@ void nrfx_clock_irq_handler(void)
     {
         nrf_clock_event_clear(NRF_CLOCK, NRF_CLOCK_EVENT_LFCLKSTARTED);
         NRFX_LOG_DEBUG("Event: NRF_CLOCK_EVENT_LFCLKSTARTED");
-        nrf_clock_int_disable(NRF_CLOCK, NRF_CLOCK_INT_LF_STARTED_MASK);
 
-        m_clock_cb.event_handler(NRFX_CLOCK_EVT_LFCLK_STARTED);
+#if NRFX_CHECK(NRFX_CLOCK_CONFIG_LFXO_TWO_STAGE_ENABLED)
+        nrf_clock_lfclk_t lfclksrc;
+        (void)nrf_clock_is_running(NRF_CLOCK, NRF_CLOCK_DOMAIN_LFCLK, &lfclksrc);
+        if (lfclksrc == NRF_CLOCK_LFCLK_RC)
+        {
+            // After the LFRC oscillator start switch to external source.
+            nrf_clock_lf_src_set(NRF_CLOCK, (nrf_clock_lfclk_t)NRFX_CLOCK_CONFIG_LF_SRC);
+            nrf_clock_task_trigger(NRF_CLOCK, NRF_CLOCK_TASK_LFCLKSTART);
+        }
+        else
+#endif
+        {
+            // After the LF clock external source start invoke user callback.
+            nrf_clock_int_disable(NRF_CLOCK, NRF_CLOCK_INT_LF_STARTED_MASK);
+            m_clock_cb.event_handler(NRFX_CLOCK_EVT_LFCLK_STARTED);
+        }
     }
 
 #if NRFX_CHECK(NRFX_CLOCK_CONFIG_LF_CAL_ENABLED)
