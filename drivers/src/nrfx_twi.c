2348a1837a6f6dabecb879c523059b53ff03fc6d
Xavier Chapron <xavier.chapron@stimio.fr>
2020-12-08 14:28:57 +0100

    drivers: nrfx_{twi, twim}: Fix busy flag not reset
    This is necessary so that next occurence of nrfx_{twi, twim}_enable()
    followed by {twi, twim}_xfer() will not raised a NRFX_ERROR_BUSY error.
    
    This could happen when a previous transfer is started but is aborted
    using nrfx_{twi, twim}_disable() before the end of the transaction which
    calls {twi, twim}_irq_handler().
    
    Signed-off-by: Xavier Chapron <xavier.chapron@stimio.fr>

--- drivers/src/nrfx_twi.c
+++ drivers/src/nrfx_twi.c
@@ -245,6 +245,7 @@ void nrfx_twi_disable(nrfx_twi_t const * p_instance)
     nrf_twi_disable(p_twi);
 
     p_cb->state = NRFX_DRV_STATE_INITIALIZED;
+    p_cb->busy = false;
     NRFX_LOG_INFO("Instance disabled: %d.", p_instance->drv_inst_idx);
 }
 
