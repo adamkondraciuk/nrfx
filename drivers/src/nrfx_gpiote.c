aa1acead98843061b4bc1b8f71a096d774553315
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2021-04-13 15:22:46 +0200

    NRFX-859: Fix mem corruption in nrfx_gpiote_in_uninit
    When assertions are disabled and user attempts to uninitialize
    pin that was not previously initialized,
    out-of-bounds memory write occurs.

--- drivers/src/nrfx_gpiote.c
+++ drivers/src/nrfx_gpiote.c
@@ -788,7 +788,10 @@ void nrfx_gpiote_in_uninit(nrfx_gpiote_pin_t pin)
         nrf_gpio_cfg_default(pin);
         pin_configured_clear(pin);
     }
-    channel_free((uint8_t)channel_port_get(pin));
+    if (pin_in_use_by_gpiote(pin))
+    {
+        channel_free((uint8_t)channel_port_get(pin));
+    }
     pin_in_use_clear(pin);
 }
 
