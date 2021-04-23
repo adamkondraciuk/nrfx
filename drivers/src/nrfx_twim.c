294b71446a2b628c64a4ffd6fa35b3a8e1912700
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2021-04-23 09:23:16 +0200

    NRFX-868: Fix nrfx_twim stalling when zero-length xfer is used

--- drivers/src/nrfx_twim.c
+++ drivers/src/nrfx_twim.c
@@ -388,7 +388,7 @@ static nrfx_err_t twim_xfer(twim_control_block_t        * p_cb,
     nrf_twim_task_t  start_task = NRF_TWIM_TASK_STARTTX;
     p_cb->error = false;
 
-    if (!nrfx_is_in_ram(p_xfer_desc->p_primary_buf))
+    if (p_xfer_desc->primary_length != 0 && !nrfx_is_in_ram(p_xfer_desc->p_primary_buf))
     {
         err_code = NRFX_ERROR_INVALID_ADDR;
         NRFX_LOG_WARNING("Function: %s, error code: %s.",
@@ -495,6 +495,10 @@ static nrfx_err_t twim_xfer(twim_control_block_t        * p_cb,
     if (!(flags & NRFX_TWIM_FLAG_HOLD_XFER) && (p_xfer_desc->type != NRFX_TWIM_XFER_TXTX))
     {
         nrf_twim_task_trigger(p_twim, start_task);
+        if (p_xfer_desc->primary_length == 0)
+        {
+            nrf_twim_task_trigger(p_twim, NRF_TWIM_TASK_STOP);
+        }
     }
 
     if (p_cb->handler)
