fa12faa30310cb4fe4977b24f2baf47b0d720176
Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>
2021-05-05 16:32:17 +0200

    NRFX-887: Add RESETINFO HAL
    Signed-off-by: Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>

---
+++ hal/nrf_resetinfo.h
@@ -0,0 +1,240 @@
+/*$$$LICENCE_NORDIC_STANDARD<2021>$$$*/
+
+#ifndef NRF_RESETINFO_H_
+#define NRF_RESETINFO_H_
+
+#include <nrfx.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup nrf_resetinfo_hal RESETINFO HAL
+ * @{
+ * @ingroup nrf_resetinfo
+ * @brief   Hardware access layer for managing the Reset information peripheral (RESETINFO).
+ */
+
+/** @brief Maximum value of error status. */
+#define NRF_RESETINFO_ERROR_STATUS_MAX RESETINFO_RESETREAS_ERROR_STATUS_ERRORSTATUS_Msk
+
+/** @brief Global reset reason mask. */
+typedef enum
+{
+    NRF_RESETINFO_REASON_GLOBAL_POR_MASK       = RESETINFO_RESETREAS_GLOBAL_RESETPOR_Msk,  /**< Reset from power on reset. */
+    NRF_RESETINFO_REASON_GLOBAL_PIN_MASK       = RESETINFO_RESETREAS_GLOBAL_RESETPIN_Msk,  /**< Reset from pin reset. */
+    NRF_RESETINFO_REASON_GLOBAL_DOG_MASK       = RESETINFO_RESETREAS_GLOBAL_DOG_Msk,       /**< Reset from the SysCtrl watchdog timer. */
+    NRF_RESETINFO_REASON_GLOBAL_CTRLAP_MASK    = RESETINFO_RESETREAS_GLOBAL_CTRLAP_Msk,    /**< Reset from CTRL-AP. */
+    NRF_RESETINFO_REASON_GLOBAL_SECSREQ_MASK   = RESETINFO_RESETREAS_GLOBAL_SECSREQ_Msk,   /**< Reset due to secure domain system reset request. */
+    NRF_RESETINFO_REASON_GLOBAL_SECWDT_MASK    = RESETINFO_RESETREAS_GLOBAL_SECWDT_Msk,    /**< Reset due to secure domain watchdog timer. */
+    NRF_RESETINFO_REASON_GLOBAL_SECLOCKUP_MASK = RESETINFO_RESETREAS_GLOBAL_SECLOCKUP_Msk, /**< Reset due to secure domain lockup. */
+    NRF_RESETINFO_REASON_GLOBAL_SECTAMPER_MASK = RESETINFO_RESETREAS_GLOBAL_SECTAMPER_Msk, /**< Reset due to secure domain tamper detected. */
+    NRF_RESETINFO_REASON_GLOBAL_GPIO_MASK      = RESETINFO_RESETREAS_GLOBAL_OFF_Msk,       /**< Reset due to wakeup from System OFF triggered by DETECT signal from GPIO. */
+    NRF_RESETINFO_REASON_GLOBAL_LPCOMP_MASK    = RESETINFO_RESETREAS_GLOBAL_LPCOMP_Msk,    /**< Reset due to wakeup from System OFF triggered by LPCOMP. */
+    NRF_RESETINFO_REASON_GLOBAL_DIF_MASK       = RESETINFO_RESETREAS_GLOBAL_DIF_Msk,       /**< Reset due to wakeup from System OFF triggered by entering debug interface mode. */ 
+    NRF_RESETINFO_REASON_GLOBAL_GRTC_MASK      = RESETINFO_RESETREAS_GLOBAL_GRTC_Msk,      /**< Reset due to wakeup from System OFF triggered by GRTC interrupt. */
+    NRF_RESETINFO_REASON_GLOBAL_NFC_MASK       = RESETINFO_RESETREAS_GLOBAL_NFC_Msk,       /**< Reset due to wakeup from System OFF triggered by NRF field detection in sense mode. */
+} nrf_resetinfo_reason_global_mask_t;
+
+/** @brief Local reset reason mask. */
+typedef enum
+{
+    NRF_RESETINFO_REASON_LOCAL_DOG_MASK         = RESETINFO_RESETREAS_LOCAL_DOG_Msk,            /**< Reset from the local watchdog timer. */
+    NRF_RESETINFO_REASON_LOCAL_DOGNS_MASK       = RESETINFO_RESETREAS_LOCAL_DOGNS_Msk,          /**< Reset from the local non-secure watchdog timer. */
+    NRF_RESETINFO_REASON_LOCAL_SREQ_MASK        = RESETINFO_RESETREAS_LOCAL_SREQ_Msk,           /**< Reset from the local soft reset request. */
+    NRF_RESETINFO_REASON_LOCAL_LOCKUP_MASK      = RESETINFO_RESETREAS_LOCAL_LOCKUP_Msk,         /**< Reset from local CPU lockup. */
+    NRF_RESETINFO_REASON_LOCAL_CROSSDOMAIN_MASK = RESETINFO_RESETREAS_LOCAL_CROSSDOMAIN_Msk,    /**< Reset due to cross domain reset source. */
+    NRF_RESETINFO_REASON_LOCAL_UNRETAINED_MASK  = RESETINFO_RESETREAS_LOCAL_UNRETAINEDWAKE_Msk, /**< Reset due to wake from unretained state. */
+} nrf_resetinfo_reason_local_mask_t;
+
+/**
+ * @brief Function for setting the global reset reason.
+ *
+ * @param[in] p_reg Pointer to the structure of registers of the peripheral.
+ * @param[in] mask  Mask of reasons to be set.
+ */
+NRF_STATIC_INLINE void nrf_resetinfo_reason_global_set(NRF_RESETINFO_Type * p_reg, uint32_t mask);
+
+/**
+ * @brief Function for getting the global reset reason.
+ *
+ * @param[in] p_reg Pointer to the structure of registers of the peripheral.
+ *
+ * @return Mask of detected global reset reasons.
+ */
+NRF_STATIC_INLINE uint32_t nrf_resetinfo_reason_global_get(NRF_RESETINFO_Type const * p_reg);
+
+/**
+ * @brief Function for setting the local reset reason.
+ *
+ * @param[in] p_reg Pointer to the structure of registers of the peripheral.
+ * @param[in] mask  Mask of reasons to be set.
+ */
+NRF_STATIC_INLINE void nrf_resetinfo_reason_local_set(NRF_RESETINFO_Type * p_reg, uint32_t mask);
+
+/**
+ * @brief Function for getting the local reset reason.
+ *
+ * @param[in] p_reg Pointer to the structure of registers of the peripheral.
+ *
+ * @return Mask of detected local reset reasons.
+ */
+NRF_STATIC_INLINE uint32_t nrf_resetinfo_reason_local_get(NRF_RESETINFO_Type const * p_reg);
+
+/**
+ * @brief Function for setting the reset error status.
+ *
+ * @param[in] p_reg  Pointer to the structure of registers of the peripheral.
+ * @param[in] status Reset error status to be set.
+ */
+NRF_STATIC_INLINE void nrf_resetinfo_error_status_set(NRF_RESETINFO_Type * p_reg, uint8_t status);
+
+/**
+ * @brief Function for getting the reset error status.
+ *
+ * @param[in] p_reg Pointer to the structure of registers of the peripheral.
+ *
+ * @return Reset error status.
+ */
+NRF_STATIC_INLINE uint8_t nrf_resetinfo_error_status_get(NRF_RESETINFO_Type const * p_reg);
+
+/**
+ * @brief Function for setting the reset error address.
+ *
+ * @param[in] p_reg   Pointer to the structure of registers of the peripheral.
+ * @param[in] address Reset error address to be set.
+ */
+NRF_STATIC_INLINE void nrf_resetinfo_error_address_set(NRF_RESETINFO_Type * p_reg,
+                                                       uint32_t             address);
+
+/**
+ * @brief Function for getting the reset error address.
+ *
+ * @param[in] p_reg Pointer to the structure of registers of the peripheral.
+ *
+ * @return Reset error address.
+ */
+NRF_STATIC_INLINE uint32_t nrf_resetinfo_error_address_get(NRF_RESETINFO_Type const * p_reg);
+
+/**
+ * @brief Function for setting the flag indicating presence of valid restore image in RAM.
+ *
+ * @param[in] p_reg  Pointer to the structure of registers of the peripheral.
+ * @param[in] enable True if valid restore image is present in RAM, false otherwise.
+ */
+NRF_STATIC_INLINE void nrf_resetinfo_restore_valid_set(NRF_RESETINFO_Type * p_reg, bool enable);
+
+/**
+ * @brief Function for getting the flag indicating presence of valid restore image in RAM.
+ *
+ * @param[in] p_reg Pointer to the structure of registers of the peripheral.
+ *
+ * @retval true  Valid restore image is present in RAM.
+ * @retval false Valid restore image is not present in RAM.
+ */
+NRF_STATIC_INLINE bool nrf_resetinfo_restore_valid_check(NRF_RESETINFO_Type const * p_reg);
+
+/**
+ * @brief Function for setting the flag indicating if CPU lockup signal is to be masked.
+ *
+ * @note Prevents the CPU lockup signal from triggering a local reset.
+ *       Needed to be able to start debug session.
+ *
+ * @param[in] p_reg  Pointer to the structure of registers of the peripheral.
+ * @param[in] enable True if lockup signal is to be masked, false otherwise.
+ */
+NRF_STATIC_INLINE void nrf_resetinfo_mask_lockup_set(NRF_RESETINFO_Type * p_reg, bool enable);
+
+/**
+ * @brief Function for getting the flag indicating if CPU lockup signal is to be masked.
+ *
+ * @note Prevents the CPU lockup signal from triggering a local reset.
+ *       Needed to be able to start debug session.
+ *
+ * @param[in] p_reg Pointer to the structure of registers of the peripheral.
+ *
+ * @retval true  CPU lockup signal is to be masked.
+ * @retval false CPU lockup signal is not to be masked.
+ */
+NRF_STATIC_INLINE bool nrf_resetinfo_mask_lockup_check(NRF_RESETINFO_Type const * p_reg);
+
+#ifndef NRF_DECLARE_ONLY
+
+NRF_STATIC_INLINE void nrf_resetinfo_reason_global_set(NRF_RESETINFO_Type * p_reg, uint32_t mask)
+{
+    p_reg->RESETREAS.GLOBAL = mask;
+}
+
+NRF_STATIC_INLINE uint32_t nrf_resetinfo_reason_global_get(NRF_RESETINFO_Type const * p_reg)
+{
+    return p_reg->RESETREAS.GLOBAL;
+}
+NRF_STATIC_INLINE void nrf_resetinfo_reason_local_set(NRF_RESETINFO_Type * p_reg, uint32_t mask)
+{
+    p_reg->RESETREAS.LOCAL = mask;
+}
+
+NRF_STATIC_INLINE uint32_t nrf_resetinfo_reason_local_get(NRF_RESETINFO_Type const * p_reg)
+{
+    return p_reg->RESETREAS.LOCAL;
+}
+
+NRF_STATIC_INLINE void nrf_resetinfo_error_status_set(NRF_RESETINFO_Type * p_reg, uint8_t status)
+{
+    NRFX_ASSERT(status <= NRF_RESETINFO_ERROR_STATUS_MAX);
+    p_reg->RESETREAS.ERROR.STATUS = status;
+}
+
+NRF_STATIC_INLINE uint8_t nrf_resetinfo_error_status_get(NRF_RESETINFO_Type const * p_reg)
+{
+    return p_reg->RESETREAS.ERROR.STATUS;
+}
+
+NRF_STATIC_INLINE void nrf_resetinfo_error_address_set(NRF_RESETINFO_Type * p_reg,
+                                                       uint32_t             address)
+{
+    p_reg->RESETREAS.ERROR.ADDRESS = address;
+}
+
+NRF_STATIC_INLINE uint32_t nrf_resetinfo_error_address_get(NRF_RESETINFO_Type const * p_reg)
+{
+    return p_reg->RESETREAS.ERROR.ADDRESS;
+}
+
+NRF_STATIC_INLINE void nrf_resetinfo_restore_valid_set(NRF_RESETINFO_Type * p_reg, bool enable)
+{
+    p_reg->RESTOREVALID = (enable ? RESETINFO_RESTOREVALID_RESTOREVALID_Present :
+                           RESETINFO_RESTOREVALID_RESTOREVALID_NotPreset)
+                          << RESETINFO_RESTOREVALID_RESTOREVALID_Pos;
+}
+
+NRF_STATIC_INLINE bool nrf_resetinfo_restore_valid_check(NRF_RESETINFO_Type const * p_reg)
+{
+    return ((p_reg->RESTOREVALID & RESETINFO_RESTOREVALID_RESTOREVALID_Msk)
+            >> RESETINFO_RESTOREVALID_RESTOREVALID_Pos)
+           == RESETINFO_RESTOREVALID_RESTOREVALID_Present;
+}
+NRF_STATIC_INLINE void nrf_resetinfo_mask_lockup_set(NRF_RESETINFO_Type * p_reg, bool enable)
+{
+    p_reg->MASKLOCKUP = (enable ? RESETINFO_MASKLOCKUP_MASK_Mask :
+                         RESETINFO_MASKLOCKUP_MASK_NotMask)
+                        << RESETINFO_MASKLOCKUP_MASK_Pos;
+}
+
+NRF_STATIC_INLINE bool nrf_resetinfo_mask_lockup_check(NRF_RESETINFO_Type const * p_reg)
+{
+    return ((p_reg->MASKLOCKUP & RESETINFO_MASKLOCKUP_MASK_Msk)
+            >> RESETINFO_MASKLOCKUP_MASK_Pos)
+           == RESETINFO_MASKLOCKUP_MASK_Mask;
+}
+
+#endif // NRF_DECLARE_ONLY
+
+/** @} */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NRF_RESETINFO_H_ */
