dafb7e8753f9bb700ea063490ec1dd3558f8eb4c
Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>
2021-05-07 10:21:41 +0200

    NRFX-871: Add IRQMAP HAL
    Signed-off-by: Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>

---
+++ hal/nrf_irqmap.h
@@ -0,0 +1,70 @@
+/*$$$LICENCE_NORDIC_STANDARD<2021>$$$*/
+
+#ifndef NRF_IRQMAP_H_
+#define NRF_IRQMAP_H_
+
+#include <nrfx.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup nrf_irqmap_hal IRQMAP HAL
+ * @{
+ * @ingroup nrf_irqmap
+ * @brief   Hardware access layer for managing the Interrupt Mapper peripheral (IRQMAP).
+ */
+
+/** @brief Number of interrupts. */
+#define NRF_IRQMAP_IRQ_COUNT IRQMAP_IRQ_MaxCount
+
+/**
+ * @brief Function for setting the interrupt sink configuration for given interrupt.
+ *
+ * @param[in] p_reg  Pointer to the structure of registers of the peripheral.
+ * @param[in] index  Interrupt index.
+ * @param[in] domain Domain indentifier to be set.
+ */
+NRF_STATIC_INLINE void nrf_irqmap_sink_set(NRF_IRQMAP_Type * p_reg,
+                                           uint32_t          index,
+                                           nrf_domain_t      domain);
+
+/**
+ * @brief Function for getting the interrupt sink configuration for given interrupt.
+ *
+ * @param[in] p_reg Pointer to the structure of registers of the peripheral.
+ * @param[in] index Interrupt index.
+ *
+ * @return Domain identifier.
+ */
+NRF_STATIC_INLINE nrf_domain_t nrf_irqmap_sink_get(NRF_IRQMAP_Type const * p_reg,
+                                                   uint32_t                index);
+
+
+#ifndef NRF_DECLARE_ONLY
+NRF_STATIC_INLINE void nrf_irqmap_sink_set(NRF_IRQMAP_Type * p_reg,
+                                           uint32_t          index,
+                                           nrf_domain_t      domain)
+{
+    NRFX_ASSERT(index < NRF_IRQMAP_IRQ_COUNT);
+    p_reg->IRQ[index].SINK = (domain << IRQMAP_IRQ_SINK_DOMAIN_Pos) 
+                             & IRQMAP_IRQ_SINK_DOMAIN_Msk;
+}
+
+NRF_STATIC_INLINE nrf_domain_t nrf_irqmap_sink_get(NRF_IRQMAP_Type const * p_reg,
+                                                   uint32_t                index)
+{
+    NRFX_ASSERT(index < NRF_IRQMAP_IRQ_COUNT);
+    return (nrf_domain_t)((p_reg->IRQ[index].SINK & IRQMAP_IRQ_SINK_DOMAIN_Msk)
+                          >> IRQMAP_IRQ_SINK_DOMAIN_Pos);
+}
+#endif // NRF_DECLARE_ONLY
+
+/** @} */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NRF_IRQMAP_H_ */
