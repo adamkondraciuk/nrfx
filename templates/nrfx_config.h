2dcf0504c2a310a37060c9b8a2e751d53e0012b6
Stephen Stauts <stephen.stauts@nordicsemi.no>
2021-06-18 10:44:13 +0200

    NRFX-NONE: Add haltium cellular nrfx config
    Adds the ability to compile cellular nrfx configs. For now, this is
    mostly a placeholder file like some other haltium configs.
    
    Ref: NCSDK-9760
    
    Signed-off-by: Stephen Stauts <stephen.stauts@nordicsemi.no>

--- templates/nrfx_config.h
+++ templates/nrfx_config.h
@@ -30,6 +30,8 @@
     #include <nrfx_config_haltium_application.h>
 #elif defined(HALTIUM_XXAA) && defined(NRF_NETWORK)
     #include <nrfx_config_haltium_network.h>
+#elif defined(HALTIUM_XXAA) && defined(NRF_CELLCORE)
+    #include <nrfx_config_haltium_cellular.h>
 #elif defined(HALTIUM_XXAA) && defined(NRF_SECURE)
     #include <nrfx_config_haltium_secure.h>
 #elif defined(HALTIUM_XXAA) && defined(NRF_FLPR)
