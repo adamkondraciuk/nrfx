b2f671a7bf2a9789653bf94612f3c407e4095e23
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2021-07-13 16:31:39 +0200

    NRFX-1037: Add nrfy_common
    Signed-off-by: Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>

---
+++ haly/nrfy_common.h
@@ -0,0 +1,99 @@
+/*$$$LICENCE_NORDIC_STANDARD<2021>$$$*/
+
+#ifndef NRFY_COMMON_H__
+#define NRFY_COMMON_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+NRFY_STATIC_INLINE bool __nrfy_internal_irq_is_enabled(int32_t irq_number);
+
+NRFY_STATIC_INLINE bool __nrfy_internal_irq_is_pending(int32_t irq_number);
+
+/**
+ * @defgroup nrfy_common Common module
+ * @{
+ * @ingroup nrfy
+ * @brief Common module.
+ */
+
+/**
+ * @brief Macro for calculating interrupt bitmask associated with the specified event.
+ *
+ * @param[in] event Event.
+ *
+ * @return Interrupt bitmask.
+ */
+#define NRFY_EVENT_TO_INT_BITMASK(event) (1 << ((((uint32_t)event) - 0x100) >> 2))
+
+/** @sa NRFX_IRQ_PRIORITY_SET */
+#define NRFY_IRQ_PRIORITY_SET(irq_number, priority)  \
+    do {                                             \
+        NRFX_IRQ_PRIORITY_SET(irq_number, priority); \
+        nrf_barrier_w();                             \
+    } while (0)
+
+/** @sa NRFX_IRQ_ENABLE */
+#define NRFY_IRQ_ENABLE(irq_number)  \
+    do {                             \
+        NRFX_IRQ_ENABLE(irq_number); \
+        nrf_barrier_w();             \
+    } while (0)
+
+/** @sa NRFX_IRQ_IS_ENABLED */
+#define NRFY_IRQ_IS_ENABLED(irq_number) __nrfy_internal_irq_is_enabled(irq_number)
+
+/** @sa NRFX_IRQ_DISABLE */
+#define NRFY_IRQ_DISABLE(irq_number)  \
+    do {                              \
+        NRFX_IRQ_DISABLE(irq_number); \
+        nrf_barrier_w();              \
+    } while (0)
+
+/** @sa NRFX_IRQ_PENDING_SET */
+#define NRFY_IRQ_PENDING_SET(irq_number)  \
+    do {                                  \
+        NRFX_IRQ_PENDING_SET(irq_number); \
+        nrf_barrier_w();                  \
+    } while (0)
+
+/** @sa NRFX_IRQ_PENDING_CLEAR */
+#define NRFY_IRQ_PENDING_CLEAR(irq_number)  \
+    do {                                    \
+        NRFX_IRQ_PENDING_CLEAR(irq_number); \
+        nrf_barrier_w();                    \
+    } while (0)
+
+/** @sa NRFX_IRQ_IS_PENDING */
+#define NRFY_IRQ_IS_PENDING(irq_number) __nrfy_internal_irq_is_pending(irq_number)
+
+/** @sa NRFX_CRITICAL_SECTION_ENTER */
+#define NRFY_CRITICAL_SECTION_ENTER() NRFX_CRITICAL_SECTION_ENTER()
+
+/** @sa NRFX_CRITICAL_SECTION_EXIT */
+#define NRFY_CRITICAL_SECTION_EXIT() NRFX_CRITICAL_SECTION_EXIT()
+
+/** @} */
+
+NRFY_STATIC_INLINE bool __nrfy_internal_irq_is_enabled(int32_t irq_number)
+{
+    nrf_barrier_rw();
+    bool is_enabled = NRFX_IRQ_IS_ENABLED(irq_number);
+    nrf_barrier_r();
+    return is_enabled;
+}
+
+NRFY_STATIC_INLINE bool __nrfy_internal_irq_is_pending(int32_t irq_number)
+{
+    nrf_barrier_rw();
+    bool is_pending = NRFX_IRQ_IS_PENDING(irq_number);
+    nrf_barrier_r();
+    return is_pending;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // NRFY_COMMON_H__
--- nrfx.h
+++ nrfx.h
@@ -7,6 +7,7 @@
 #include <drivers/nrfx_common.h>
 #include <nrfx_glue.h>
 #include <hal/nrf_common.h>
+#include <haly/nrfy_common.h>
 #include <drivers/nrfx_errors.h>
 
 #if NRFX_CHECK(NRFX_PM_ENABLED)
