195e70c0a1aa4412f326b275a04ffbe735bd1c5a
Rafał Kuźnia <rafal.kuznia@nordicsemi.no>
2021-09-20 11:36:52 +0200

    NRFX-NONE: Add MAXCNT define for UARTE136
    The UARTE136_EASYDMA_MAXCNT_SIZE is missing in the MDK.
    Add it to hal/nrf_uarte.h as a workaround.
    
    Signed-off-by: Rafał Kuźnia <rafal.kuznia@nordicsemi.no>

--- hal/nrf_uarte.h
+++ hal/nrf_uarte.h
@@ -11,6 +11,9 @@ extern "C" {
 
 #define NRF_UARTE_PSEL_DISCONNECTED 0xFFFFFFFF
 
+/* TODO: remove after the symbol is available in the MDK. */
+#define UARTE136_EASYDMA_MAXCNT_SIZE 8
+
 /**
  * @defgroup nrf_uarte_hal UARTE HAL
  * @{
