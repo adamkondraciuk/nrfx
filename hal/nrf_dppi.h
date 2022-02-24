bb2b90d4429e9bc24b856d29b9a538511807ba82
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2022-02-24 17:30:32 +0100

    NRFX-1335: Add minimal alignment of nrf_dppi to Haltium
    Signed-off-by: Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>

--- hal/nrf_dppi.h
+++ hal/nrf_dppi.h
@@ -17,6 +17,13 @@ extern "C" {
  *          Interconnect Controller (DPPIC).
  */
 
+#if defined(HALTIUM_XXAA)
+/* TODO: Delete when added to MDK. */
+#define DPPI_PRESENT 1
+#define DPPI_CH_NUM 8
+#define DPPI_GROUP_NUM 2
+#endif
+
 /** @brief DPPI channel groups. */
 typedef enum
 {
@@ -35,6 +42,7 @@ typedef enum
     NRF_DPPI_TASK_CHG0_DIS = offsetof(NRF_DPPIC_Type, TASKS_CHG[0].DIS), /**< Disable channel group 0. */
     NRF_DPPI_TASK_CHG1_EN  = offsetof(NRF_DPPIC_Type, TASKS_CHG[1].EN),  /**< Enable channel group 1. */
     NRF_DPPI_TASK_CHG1_DIS = offsetof(NRF_DPPIC_Type, TASKS_CHG[1].DIS), /**< Disable channel group 1. */
+#if DPPI_GROUP_NUM > 2
     NRF_DPPI_TASK_CHG2_EN  = offsetof(NRF_DPPIC_Type, TASKS_CHG[2].EN),  /**< Enable channel group 2. */
     NRF_DPPI_TASK_CHG2_DIS = offsetof(NRF_DPPIC_Type, TASKS_CHG[2].DIS), /**< Disable channel group 2. */
     NRF_DPPI_TASK_CHG3_EN  = offsetof(NRF_DPPIC_Type, TASKS_CHG[3].EN),  /**< Enable channel group 3. */
@@ -43,6 +51,7 @@ typedef enum
     NRF_DPPI_TASK_CHG4_DIS = offsetof(NRF_DPPIC_Type, TASKS_CHG[4].DIS), /**< Disable channel group 4. */
     NRF_DPPI_TASK_CHG5_EN  = offsetof(NRF_DPPIC_Type, TASKS_CHG[5].EN),  /**< Enable channel group 5. */
     NRF_DPPI_TASK_CHG5_DIS = offsetof(NRF_DPPIC_Type, TASKS_CHG[5].DIS)  /**< Disable channel group 5. */
+#endif
 } nrf_dppi_task_t;
 
 /**
