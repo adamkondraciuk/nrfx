58786d8c7f86627fd265a3d165a2b1e912b3d34d
Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>
2023-02-08 12:34:26 +0100

    NRFX-3104: Hide Haltium/Moonlight specific IRQ configuration
    Signed-off-by: Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>

--- soc/nrfx_irqs.h
+++ soc/nrfx_irqs.h
@@ -25,14 +25,8 @@
     #include <soc/nrfx_irqs_nrf5340_network.h>
 #elif defined(NRF91_SERIES)
     #include <soc/nrfx_irqs_nrf91.h>
-#elif defined(HALTIUM_XXAA) && !defined(NRF9230_XXAA)
-    #include <soc/nrfx_irqs_haltium.h>
-#elif defined(NRF9230_XXAA)
-    #include <soc/nrfx_irqs_nrf9230.h>
-#elif defined(MOONLIGHT_XXAA)
-    #include <soc/nrfx_irqs_moonlight.h>
 #else
-    #error "Unknown device."
+    #include <soc/nrfx_irqs_ext.h>
 #endif
 
 #endif // NRFX_IRQS_H__
---
+++ soc/nrfx_irqs_ext.h
@@ -0,0 +1,16 @@
+/*$$$LICENCE_NORDIC_STANDARD<2023>$$$*/
+
+#ifndef NRFX_IRQS_EXT_H__
+#define NRFX_IRQS_EXT_H__
+
+#if defined(HALTIUM_XXAA) && !defined(NRF9230_XXAA)
+    #include <soc/nrfx_irqs_haltium.h>
+#elif defined(NRF9230_XXAA)
+    #include <soc/nrfx_irqs_nrf9230.h>
+#elif defined(MOONLIGHT_XXAA)
+    #include <soc/nrfx_irqs_moonlight.h>
+#else
+    #error "Unknown device."
+#endif
+
+#endif // NRFX_IRQS_EXT_H__
