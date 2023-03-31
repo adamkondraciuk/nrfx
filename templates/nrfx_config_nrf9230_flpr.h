a959247795decbeb4ddb177004a3ce660badde7d
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2023-03-31 10:47:41 +0200

    NRFX-3333: nrfx_config: Remove GPIOTE from FLPR, UMAC and LMAC
    Above-mentioned cores cannot use GPIOTE. Remove it from config.

--- templates/nrfx_config_nrf9230_flpr.h
+++ templates/nrfx_config_nrf9230_flpr.h
@@ -232,57 +232,6 @@
 #endif
 
 /**
- * @brief NRFX_GPIOTE_ENABLED
- *
- * Boolean. Accepted values 0 and 1.
- */
-#ifndef NRFX_GPIOTE_ENABLED
-#define NRFX_GPIOTE_ENABLED 0
-#endif
-
-/**
- * @brief NRFX_GPIOTE_DEFAULT_CONFIG_IRQ_PRIORITY
- *
- * Integer value. Minimum: 0 Maximum: 3
- */
-#ifndef NRFX_GPIOTE_DEFAULT_CONFIG_IRQ_PRIORITY
-#define NRFX_GPIOTE_DEFAULT_CONFIG_IRQ_PRIORITY 3
-#endif
-
-/**
- * @brief NRFX_GPIOTE_CONFIG_NUM_OF_EVT_HANDLERS
- *
- * Integer value. Minimum: 0 Maximum: 15
- */
-#ifndef NRFX_GPIOTE_CONFIG_NUM_OF_EVT_HANDLERS
-#define NRFX_GPIOTE_CONFIG_NUM_OF_EVT_HANDLERS 2
-#endif
-
-/**
- * @brief NRFX_GPIOTE_CONFIG_LOG_ENABLED
- *
- * Boolean. Accepted values 0 and 1.
- */
-#ifndef NRFX_GPIOTE_CONFIG_LOG_ENABLED
-#define NRFX_GPIOTE_CONFIG_LOG_ENABLED 0
-#endif
-
-/**
- * @brief NRFX_GPIOTE_CONFIG_LOG_LEVEL
- *
- * Integer value.
- * Supported values:
- * - Off     = 0
- * - Error   = 1
- * - Warning = 2
- * - Info    = 3
- * - Debug   = 4
- */
-#ifndef NRFX_GPIOTE_CONFIG_LOG_LEVEL
-#define NRFX_GPIOTE_CONFIG_LOG_LEVEL 3
-#endif
-
-/**
  * @brief NRFX_GRTC_ENABLED
  *
  * Boolean. Accepted values 0 and 1.
