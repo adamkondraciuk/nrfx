d769ca9e66a8babd7401ad20723967835f7ae4b9
Natalia Pluta <natalia.pluta@nordicsemi.no>
2023-04-11 13:14:08 +0200

    NRFX-3451: Remove redundant nrf_lpcomp_pin_convert function
    Signed-off-by: Natalia Pluta <natalia.pluta@nordicsemi.no>

--- hal/nrf_lpcomp.h
+++ hal/nrf_lpcomp.h
@@ -16,11 +16,11 @@ extern "C" {
  * @brief   Hardware access layer for managing the Low Power Comparator (LPCOMP) peripheral.
  */
 
-#if defined(LPCOMP_PSEL_PSEL_AnalogInput0) || defined(__NRFX_DOXYGEN__)
-/** @brief Symbol indicating whether LPCOMP uses enum for pins. */
-#define NRF_LPCOMP_HAS_PIN_ENUM 1
+#if defined(LPCOMP_PSEL_PIN_Msk) || defined(__NRFX_DOXYGEN__)
+/** @brief Symbol indicating whether the configuration of analog input using pin number is present. */
+#define NRF_LPCOMP_HAS_AIN_AS_PIN 1
 #else
-#define NRF_LPCOMP_HAS_PIN_ENUM 0
+#define NRF_LPCOMP_HAS_AIN_AS_PIN 0
 #endif
 
 /** @brief LPCOMP reference selection. */
@@ -51,7 +51,7 @@ typedef enum
     NRF_LPCOMP_REF_SUPPLY_13_16 = LPCOMP_REFSEL_REFSEL_Ref13_16Vdd,                    /**< Use supply with a 13/16 prescaler as reference. */
     NRF_LPCOMP_REF_SUPPLY_15_16 = LPCOMP_REFSEL_REFSEL_Ref15_16Vdd,                    /**< Use supply with a 15/16 prescaler as reference. */
 #endif
-#if NRF_LPCOMP_HAS_PIN_ENUM
+#if !NRF_LPCOMP_HAS_AIN_AS_PIN
     NRF_LPCOMP_REF_EXT_REF0     = LPCOMP_REFSEL_REFSEL_ARef |
                                   (LPCOMP_EXTREFSEL_EXTREFSEL_AnalogReference0 << 16), /**< External reference 0. */
     NRF_LPCOMP_REF_EXT_REF1     = LPCOMP_REFSEL_REFSEL_ARef |
@@ -60,7 +60,9 @@ typedef enum
 } nrf_lpcomp_ref_t;
 
 /** @brief LPCOMP input selection. */
-#if NRF_LPCOMP_HAS_PIN_ENUM
+#if NRF_LPCOMP_HAS_AIN_AS_PIN
+typedef uint32_t nrf_lpcomp_input_t;
+#else
 typedef enum
 {
     NRF_LPCOMP_INPUT_0 = LPCOMP_PSEL_PSEL_AnalogInput0, /**< Input 0. */
@@ -72,8 +74,6 @@ typedef enum
     NRF_LPCOMP_INPUT_6 = LPCOMP_PSEL_PSEL_AnalogInput6, /**< Input 6. */
     NRF_LPCOMP_INPUT_7 = LPCOMP_PSEL_PSEL_AnalogInput7  /**< Input 7. */
 } nrf_lpcomp_input_t;
-#else
-typedef uint16_t nrf_lpcomp_input_t;
 #endif
 
 /** @brief LPCOMP detection type selection. */
@@ -296,18 +296,6 @@ NRF_STATIC_INLINE void nrf_lpcomp_event_clear(NRF_LPCOMP_Type * p_reg, nrf_lpcom
 NRF_STATIC_INLINE bool nrf_lpcomp_event_check(NRF_LPCOMP_Type const * p_reg,
                                               nrf_lpcomp_event_t      event);
 
-#if !NRF_LPCOMP_HAS_PIN_ENUM
-/**
- * @brief Function for mapping GPIO pins to LPCOMP pins.
- *
- * @param[in] port Port of the GPIO pin.
- * @param[in] pin  GPIO pin number.
- *
- * @return LPCOMP reference pin to be used in @ref nrf_lpcomp_input_select.
- */
-NRF_STATIC_INLINE uint16_t nrf_lpcomp_pin_convert(uint8_t port, uint8_t pin);
-#endif
-
 #ifndef NRF_DECLARE_ONLY
 
 NRF_STATIC_INLINE void nrf_lpcomp_configure(NRF_LPCOMP_Type *           p_reg,
@@ -315,7 +303,7 @@ NRF_STATIC_INLINE void nrf_lpcomp_configure(NRF_LPCOMP_Type *           p_reg,
 {
     p_reg->REFSEL = (p_config->reference << LPCOMP_REFSEL_REFSEL_Pos) & LPCOMP_REFSEL_REFSEL_Msk;
 
-#if NRF_LPCOMP_HAS_PIN_ENUM
+#if !NRF_LPCOMP_HAS_AIN_AS_PIN
     //If external source is choosen extract analog reference index.
     if ((p_config->reference & LPCOMP_REFSEL_REFSEL_ARef)==LPCOMP_REFSEL_REFSEL_ARef)
     {
@@ -334,11 +322,12 @@ NRF_STATIC_INLINE void nrf_lpcomp_configure(NRF_LPCOMP_Type *           p_reg,
 
 NRF_STATIC_INLINE void nrf_lpcomp_input_select(NRF_LPCOMP_Type * p_reg, nrf_lpcomp_input_t input)
 {
-#if NRF_LPCOMP_HAS_PIN_ENUM
-    p_reg->PSEL   = ((uint32_t)input << LPCOMP_PSEL_PSEL_Pos) |
-                    (p_reg->PSEL & ~LPCOMP_PSEL_PSEL_Msk);
+#if NRF_LPCOMP_HAS_AIN_AS_PIN
+    p_reg->PSEL = (NRF_PIN_NUMBER_TO_PORT(input) << LPCOMP_PSEL_PORT_Pos) |
+                  (NRF_PIN_NUMBER_TO_PIN(input) << LPCOMP_PSEL_PIN_Pos) |
+                  (p_reg->PSEL & ~(LPCOMP_PSEL_PORT_Msk | LPCOMP_PSEL_PIN_Msk));
 #else
-    p_reg->PSEL = input;
+    p_reg->PSEL = ((uint32_t)input << LPCOMP_PSEL_PSEL_Pos) | (p_reg->PSEL & ~LPCOMP_PSEL_PSEL_Msk);
 #endif
 }
 
@@ -411,14 +400,6 @@ NRF_STATIC_INLINE bool nrf_lpcomp_event_check(NRF_LPCOMP_Type const * p_reg,
 {
     return (bool) (*(volatile uint32_t *)( (uint8_t *)p_reg + (uint32_t)event));
 }
-
-#if !NRF_LPCOMP_HAS_PIN_ENUM
-NRF_STATIC_INLINE uint16_t nrf_lpcomp_pin_convert(uint8_t port, uint8_t pin)
-{
-    return (((uint16_t)port << LPCOMP_PSEL_PORT_Pos) | ((uint16_t)pin << LPCOMP_PSEL_PIN_Pos));
-}
-#endif
-
 
 #endif // NRF_DECLARE_ONLY
 
