5cb7d2fe48a05bcd125ddd0d8eea4861d6ad4442
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2023-06-15 11:17:35 +0200

    NRFX-NONE: nrf_hsfll: Flip MIRROR register logic
    Previously mirror lock logic was flipped but it was a
    misinterpretation of the documentation combained with specific
    behavior when closed loop mode is enabled (with closed loop
    applying FICR with mirror locked was not giving expected
    frequency).
    
    After discussion with Tarpan and SAG it has been clarified
    that when MIRROR lock is disabled then registers are applied
    immediately one by one vs batch appling when lock was enabled
    during setting and disabled after all registers have been
    updated.

--- hal/nrf_hsfll.h
+++ hal/nrf_hsfll.h
@@ -577,13 +577,13 @@ NRF_STATIC_INLINE void nrf_hsfll_trim_set(NRF_HSFLL_Type *         p_reg,
                                           nrf_hsfll_trim_t const * p_trim)
 {
     NRFX_ASSERT(p_trim);
-    nrf_hsfll_mirror_lock_set(p_reg, false);
+    nrf_hsfll_mirror_lock_set(p_reg, true);
 
     p_reg->TRIM.VSUP   = p_trim->vsup;
     p_reg->TRIM.COARSE = p_trim->coarse;
     p_reg->TRIM.FINE   = p_trim->fine;
 
-    nrf_hsfll_mirror_lock_set(p_reg, true);
+    nrf_hsfll_mirror_lock_set(p_reg, false);
 }
 
 NRF_STATIC_INLINE void nrf_hsfll_trim_get(NRF_HSFLL_Type const * p_reg,
