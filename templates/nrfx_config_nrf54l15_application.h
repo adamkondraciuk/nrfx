e29d535670f618b8bfe529ee78da634d303f2385
Jakub Zymelka <jakub.zymelka@nordicsemi.no>
2023-07-02 23:46:50 +0200

    NRFX-2577: Add RRAMC instance for nrfx_config file for nRF54L15
    Signed-off-by: Jakub Zymelka <jakub.zymelka@nordicsemi.no>

--- templates/nrfx_config_nrf54l15_application.h
+++ templates/nrfx_config_nrf54l15_application.h
@@ -587,6 +587,48 @@
 #endif
 
 /**
+ * @brief NRFX_RRAMC_ENABLED
+ *
+ * Boolean. Accepted values 0 and 1.
+ */
+#ifndef NRFX_RRAMC_ENABLED
+#define NRFX_RRAMC_ENABLED 0
+#endif
+
+/**
+ * @brief NRFX_RRAMC_DEFAULT_CONFIG_IRQ_PRIORITY
+ *
+ * Integer value. Minimum: 0 Maximum: 7
+ */
+#ifndef NRFX_RRAMC_DEFAULT_CONFIG_IRQ_PRIORITY
+#define NRFX_RRAMC_DEFAULT_CONFIG_IRQ_PRIORITY NRFX_DEFAULT_IRQ_PRIORITY
+#endif
+
+/**
+ * @brief NRFX_RRAMC_CONFIG_LOG_ENABLED
+ *
+ * Boolean. Accepted values 0 and 1.
+ */
+#ifndef NRFX_RRAMC_CONFIG_LOG_ENABLED
+#define NRFX_RRAMC_CONFIG_LOG_ENABLED 0
+#endif
+
+/**
+ * @brief NRFX_RRAMC_CONFIG_LOG_LEVEL
+ *
+ * Integer value.
+ * Supported values:
+ * - Off     = 0
+ * - Error   = 1
+ * - Warning = 2
+ * - Info    = 3
+ * - Debug   = 4
+ */
+#ifndef NRFX_RRAMC_CONFIG_LOG_LEVEL
+#define NRFX_RRAMC_CONFIG_LOG_LEVEL 3
+#endif
+
+/**
  * @brief NRFX_RTC_ENABLED
  *
  * Boolean. Accepted values: 0 and 1.
