44df9b1ccc60c89891f791a244de76f101d30be7
Natalia Pluta <natalia.pluta@nordicsemi.no>
2023-07-31 11:09:32 +0200

    NRFX-3172: Remove deprecated VPRCSR_MINTTHRESH reg workaround
    Signed-off-by: Natalia Pluta <natalia.pluta@nordicsemi.no>

--- hal/nrf_vpr_csr.h
+++ hal/nrf_vpr_csr.h
@@ -401,22 +401,12 @@ NRF_STATIC_INLINE uint32_t nrf_vpr_csr_machine_trap_value_get(void)
 
 NRF_STATIC_INLINE void nrf_vpr_csr_machine_interrupt_threshold_set(uint8_t th)
 {
-    // TODO: [NRFX-3172] Remove when FPGA and Palladium will have VPR1.1 implemented (see IP-5053)
-#if defined(BOARD_SYSTEMC)
-    nrf_csr_write(VPRCSR_MINTTHRESH, th);
-#else
     nrf_csr_write(VPRCSR_MINTTHRESH, (th << VPRCSR_MINTTHRESH_TH_Pos));
-#endif
 }
 
 NRF_STATIC_INLINE uint8_t nrf_vpr_csr_machine_interrupt_threshold_get(void)
 {
-    // TODO: [NRFX-3172] Remove when FPGA and Palladium will have VPR1.1 implemented (see IP-5053)
-#if defined(BOARD_SYSTEMC)
-    return (uint8_t)nrf_csr_read(VPRCSR_MINTTHRESH);
-#else
     return (nrf_csr_read(VPRCSR_MINTTHRESH) & VPRCSR_MINTTHRESH_TH_Msk) >> VPRCSR_MINTTHRESH_TH_Pos;
-#endif
 }
 
 NRF_STATIC_INLINE void nrf_vpr_csr_machine_cycle_counter_enable_set(bool enable)
