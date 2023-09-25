61f500b6cb5c00053677a422e2aa8858abb7a649
Adam Wojasinski <adam.wojasinski@nordicsemi.no>
2023-09-25 08:57:33 +0200

    NRFX-4527: Add enable_check function to UARTE HAL
    Signed-off-by: Adam Wojasinski <adam.wojasinski@nordicsemi.no>

--- hal/nrf_uarte.h
+++ hal/nrf_uarte.h
@@ -361,6 +361,16 @@ NRF_STATIC_INLINE void nrf_uarte_enable(NRF_UARTE_Type * p_reg);
 NRF_STATIC_INLINE void nrf_uarte_disable(NRF_UARTE_Type * p_reg);
 
 /**
+ * @brief Function for checking if the UARTE is enabled.
+ *
+ * @param[in] p_reg Pointer to the structure of registers of the peripheral.
+ *
+ * @retval true  The UARTE is enabled.
+ * @retval false The UARTE is not enabled.
+ */
+NRF_STATIC_INLINE bool nrf_uarte_enable_check(NRF_UARTE_Type const * p_reg);
+
+/**
  * @brief Function for configuring TX/RX pins.
  *
  * @param[in] p_reg   Pointer to the structure of registers of the peripheral.
@@ -627,6 +637,11 @@ NRF_STATIC_INLINE void nrf_uarte_enable(NRF_UARTE_Type * p_reg)
 NRF_STATIC_INLINE void nrf_uarte_disable(NRF_UARTE_Type * p_reg)
 {
     p_reg->ENABLE = UARTE_ENABLE_ENABLE_Disabled;
+}
+
+NRF_STATIC_INLINE bool nrf_uarte_enable_check(NRF_UARTE_Type const * p_reg)
+{
+    return p_reg->ENABLE == UARTE_ENABLE_ENABLE_Enabled;
 }
 
 NRF_STATIC_INLINE void nrf_uarte_txrx_pins_set(NRF_UARTE_Type * p_reg,
