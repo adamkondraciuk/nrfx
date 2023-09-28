6949069f1908e52a756fa6c5e51d37c3dbc1ceec
Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>
2023-09-28 11:02:32 +0200

    NRFX-4388: fix undefined error in `nrfx_atomic`
    Signed-off-by: Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>

--- soc/nrfx_atomic.c
+++ soc/nrfx_atomic.c
@@ -1,7 +1,7 @@
 /*$$$LICENCE_NORDIC_STANDARD<2018>$$$*/
 #include "nrfx_atomic.h"
 
-#if ((__CORTEX_M >= 0x03U) || (__CORTEX_SC >= 300U))
+#if NRFX_CHECK(ISA_ARM) && ((__CORTEX_M >= 0x03U) || (__CORTEX_SC >= 300U))
 #define NRFX_ATOMIC_STREX_LDREX_PRESENT
 #else
 /* Cortex-M0 does not have build-in atomic function. Force disabling to avoid linking failure. */
