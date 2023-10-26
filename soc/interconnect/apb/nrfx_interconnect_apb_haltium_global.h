fc11d90771ea7cfc72d122cfe6483d2dda5a9aeb
Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>
2023-10-26 09:42:49 +0200

    NRFX-4858: Revert DPPIC owned channel extension
    Reverts result of NRFX-4686 due to long author absence in the nearest future.
    This reverts commit f3c3123b4cf608829bd32f6f0483be129471a1b7.
    
    Signed-off-by: Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>

--- soc/interconnect/apb/nrfx_interconnect_apb_haltium_global.h
+++ soc/interconnect/apb/nrfx_interconnect_apb_haltium_global.h
@@ -10,21 +10,11 @@ extern "C" {
 /* Instance of the DPPI peripheral which belongs to the main APB bus. */
 #define NRFX_INTERCONNECT_MAIN_DPPI_INSTANCE 130
 
-#define NRFX_DPPI_CHANNELS_VAR(inst_num) \
-        NRFX_CONCAT(m_dppi, inst_num, _channels)
-
 #ifndef NRFX_INTERCONNECT_APB_GLOBAL_DPPI_DEFINE
 /* Default DPPI static variables generation in case of bare-metal application. */
 #ifndef NRFX_DPPI_CHANNELS_SINGLE_VAR_NAME_BY_INST_NUM
 #define NRFX_DPPI_CHANNELS_SINGLE_VAR_NAME_BY_INST_NUM(inst_num) \
         NRFX_CONCAT(m_dppi, inst_num, _channels)
-#else
-#error Invalid set of configuration for DPPIC.
-#endif
-
-#ifndef NRFX_DPPI_CONFIG_OWNED_CHANNELS_MASK_BY_INST_NUM
-#define NRFX_DPPI_CONFIG_OWNED_CHANNELS_MASK_BY_INST_NUM(inst_num) \
-        NRFX_CONCAT(NRFX_DPPI, inst_num, _CONFIG_OWNED_CHANNELS_MASK)
 #else
 #error Invalid set of configuration for DPPIC.
 #endif
@@ -43,20 +33,27 @@ extern "C" {
 #error "Invalid set of configuration for DPPIC."
 #endif
 
-/* Macro checks if owned inst_num mask is not zero and then returns 1, else 0 */
+/* Macro checks if any inst_num mask (PUB or SUB) is not zero and then returns 1, else 0 */
 #ifdef NRF_SECURE
-#define NRFX_DPPI_OWNED_MASK(inst_num) 1
+#define NRFX_DPPI_PUB_OR_SUB_MASK(inst_num) 1
 #else
-#define NRFX_DPPI_OWNED_MASK(inst_num) \
-    NRFX_COND_CODE_0(NRFX_DPPI_CONFIG_OWNED_CHANNELS_MASK_BY_INST_NUM(inst_num), (0), (1))
+#define NRFX_DPPI_PUB_OR_SUB_MASK(inst_num) \
+    NRFX_COND_CODE_0(NRFX_DPPI_PUB_CONFIG_ALLOWED_CHANNELS_MASK_BY_INST_NUM(inst_num), \
+        (NRFX_COND_CODE_0(NRFX_DPPI_SUB_CONFIG_ALLOWED_CHANNELS_MASK_BY_INST_NUM(inst_num), \
+                          (0), (1))), (1))
 #endif
 
-#define NRFX_DPPI_CHANNELS_ENTRY(inst_num)                                                \
-    NRFX_COND_CODE_1(NRFX_DPPI_OWNED_MASK(inst_num),                                      \
-        (                                                                                 \
+#define NRFX_DPPI_CHANNELS_VAR(inst_num) \
+    NRFX_CONCAT(m_dppi, inst_num, _channels)
+
+#define NRFX_DPPI_CHANNELS_ENTRY(inst_num)                                            \
+    NRFX_COND_CODE_1(NRFX_DPPI_PUB_OR_SUB_MASK(inst_num), \
+        ( \
             static nrfx_atomic_t NRFX_DPPI_CHANNELS_VAR(inst_num) __attribute__((used)) = \
-            NRFX_DPPI_CONFIG_OWNED_CHANNELS_MASK_BY_INST_NUM(inst_num);                   \
+            NRFX_DPPI_PUB_CONFIG_ALLOWED_CHANNELS_MASK_BY_INST_NUM(inst_num) |           \
+            NRFX_DPPI_SUB_CONFIG_ALLOWED_CHANNELS_MASK_BY_INST_NUM(inst_num); \
         ), ())
+
 
 #define _NRFX_DPPI_CHANNELS_ENTRY(periph_name, prefix, inst_num, _)                               \
     /* Since this is used only for global instances macro shall skip instances starting with 0 */ \
@@ -68,17 +65,17 @@ extern "C" {
 #endif // NRFX_INTERCONNECT_APB_GLOBAL_DPPI_DEFINE
 
 #define NRFX_INTERCONNECT_APB_PROP_ENTRY(inst_num, ppib, apb_size)                                  \
-NRFX_COND_CODE_1(NRFX_DPPI_OWNED_MASK(inst_num),                                                \
-(                                                                                               \
-{                                                                                               \
-    .p_dppi = NRFX_CONCAT(NRF_DPPIC, inst_num),                                                 \
-    .p_dppi_channels = &NRFX_DPPI_CHANNELS_SINGLE_VAR_NAME_BY_INST_NUM(inst_num),               \
-    .dppi_pub_channels_mask = NRFX_DPPI_PUB_CONFIG_ALLOWED_CHANNELS_MASK_BY_INST_NUM(inst_num), \
-    .dppi_sub_channels_mask = NRFX_DPPI_SUB_CONFIG_ALLOWED_CHANNELS_MASK_BY_INST_NUM(inst_num), \
-    .p_ppib = ppib,                                                                             \
-    .size = apb_size,                                                                           \
-}, \
-), ())
+    NRFX_COND_CODE_1(NRFX_DPPI_PUB_OR_SUB_MASK(inst_num), \
+    ( \
+    {                                                                                               \
+        .p_dppi = NRFX_CONCAT(NRF_DPPIC, inst_num),                                                 \
+        .p_dppi_channels = &NRFX_DPPI_CHANNELS_SINGLE_VAR_NAME_BY_INST_NUM(inst_num),               \
+        .dppi_pub_channels_mask = NRFX_DPPI_PUB_CONFIG_ALLOWED_CHANNELS_MASK_BY_INST_NUM(inst_num), \
+        .dppi_sub_channels_mask = NRFX_DPPI_SUB_CONFIG_ALLOWED_CHANNELS_MASK_BY_INST_NUM(inst_num), \
+        .p_ppib = ppib,                                                                             \
+        .size = apb_size,                                                                           \
+    }, \
+    ), ())
 
 /* TODO: Use `NRFX_FOREACH_ENABLED` macro here as well when it will be possible to
          retrieve `size` parameter by instance number. There is also need to use `p_ppib` field
