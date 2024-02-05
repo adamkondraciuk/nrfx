f57d8971a314b275ee54440ec416bb2575182389
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2024-02-05 14:39:46 +0100

    NRFX-5313: Add nrfx_config templates for nRF54L15 FP1

--- templates/nrfx_config.h
+++ templates/nrfx_config.h
@@ -40,6 +40,10 @@
     #include <nrfx_config_nrf54h20_enga_ppr.h>
 #elif defined(NRF54H20_ENGA_XXAA) && defined(NRF_FLPR)
     #include <nrfx_config_nrf54h20_enga_flpr.h>
+#elif defined(NRF54L15_XXAA) && defined(NRF_APPLICATION)
+    #include <nrfx_config_nrf54l15_application.h>
+#elif defined(NRF54L15_XXAA) && defined(NRF_FLPR)
+    #include <nrfx_config_nrf54l15_flpr.h>
 #elif defined(NRF54L15_ENGA_XXAA) && defined(NRF_APPLICATION)
     #include <nrfx_config_nrf54l15_enga_application.h>
 #elif defined(NRF54L15_ENGA_XXAA) && defined(NRF_FLPR)
