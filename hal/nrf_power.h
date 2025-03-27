adamkondraciuk/nrfx

commit ae3e79a0ae38a8f81ed78dd599095b35e0924fd7

NRFX-6953: Add VMC retention symbols for bitshifting.
Add symbols for nrf_vmc and nrf_power for shifting values to
serve as masks. Utilize them in nrfx_ram_ctrl helper.

Signed-off-by: Michał Stasiak <michal.stasiak@nordicsemi.no>

hal/nrf_power.h
@@ -441,6 +441,9 @@ typedef enum
 #if defined(POWER_RAM_POWER_S0POWER_Msk) || defined(__NRFX_DOXYGEN__)
 /**
  * @brief Bit positions for RAMPOWER register
+ * 
+ * @deprecated Use @ref NRF_POWER_RAMPOWER_S0POWER_POS or 
+ *             NRF_POWER_RAMPOWER_S0RETENTION_POS instead.
  *
  * All possible bits described, even if they are not used in selected MCU.
  */
@@ -483,6 +486,12 @@ typedef enum
     NRF_POWER_RAMPOWER_S15RETENTION, /**< Keep section retention in OFF mode when section is OFF. */
 } nrf_power_rampower_t;
 
+/** @brief Position of power configuration bits for RAM section 0. */
+#define NRF_POWER_RAMPOWER_S0POWER_POS POWER_RAM_POWER_S0POWER_Pos
+
+/** @brief Position of retention configuration bits for RAM section 0. */
+#define NRF_POWER_RAMPOWER_S0RETENTION_POS POWER_RAM_POWER_S0RETENTION_Pos
+
 /**
  * @brief Bit masks for RAMPOWER register
  *
@@ -490,37 +499,95 @@ typedef enum
  */
 typedef enum
 {
-    NRF_POWER_RAMPOWER_S0POWER_MASK  = 1UL << NRF_POWER_RAMPOWER_S0POWER ,
-    NRF_POWER_RAMPOWER_S1POWER_MASK  = 1UL << NRF_POWER_RAMPOWER_S1POWER ,
-    NRF_POWER_RAMPOWER_S2POWER_MASK  = 1UL << NRF_POWER_RAMPOWER_S2POWER ,
-    NRF_POWER_RAMPOWER_S3POWER_MASK  = 1UL << NRF_POWER_RAMPOWER_S3POWER ,
-    NRF_POWER_RAMPOWER_S4POWER_MASK  = 1UL << NRF_POWER_RAMPOWER_S4POWER ,
-    NRF_POWER_RAMPOWER_S5POWER_MASK  = 1UL << NRF_POWER_RAMPOWER_S5POWER ,
-    NRF_POWER_RAMPOWER_S7POWER_MASK  = 1UL << NRF_POWER_RAMPOWER_S7POWER ,
-    NRF_POWER_RAMPOWER_S8POWER_MASK  = 1UL << NRF_POWER_RAMPOWER_S8POWER ,
-    NRF_POWER_RAMPOWER_S9POWER_MASK  = 1UL << NRF_POWER_RAMPOWER_S9POWER ,
-    NRF_POWER_RAMPOWER_S10POWER_MASK = 1UL << NRF_POWER_RAMPOWER_S10POWER,
-    NRF_POWER_RAMPOWER_S11POWER_MASK = 1UL << NRF_POWER_RAMPOWER_S11POWER,
-    NRF_POWER_RAMPOWER_S12POWER_MASK = 1UL << NRF_POWER_RAMPOWER_S12POWER,
-    NRF_POWER_RAMPOWER_S13POWER_MASK = 1UL << NRF_POWER_RAMPOWER_S13POWER,
-    NRF_POWER_RAMPOWER_S14POWER_MASK = 1UL << NRF_POWER_RAMPOWER_S14POWER,
-    NRF_POWER_RAMPOWER_S15POWER_MASK = 1UL << NRF_POWER_RAMPOWER_S15POWER,
+    NRF_POWER_RAMPOWER_S0POWER_MASK  = POWER_RAM_POWER_S0POWER_Msk,
+    NRF_POWER_RAMPOWER_S1POWER_MASK  = POWER_RAM_POWER_S1POWER_Msk,
+#if defined(POWER_RAM_POWER_S2POWER_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S2POWER_MASK  = POWER_RAM_POWER_S2POWER_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S3POWER_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S3POWER_MASK  = POWER_RAM_POWER_S3POWER_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S4POWER_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S4POWER_MASK  = POWER_RAM_POWER_S4POWER_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S5POWER_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S5POWER_MASK  = POWER_RAM_POWER_S5POWER_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S6POWER_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S6POWER_MASK  = POWER_RAM_POWER_S6POWER_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S7POWER_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S7POWER_MASK  = POWER_RAM_POWER_S7POWER_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S8POWER_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S8POWER_MASK  = POWER_RAM_POWER_S8POWER_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S9POWER_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S9POWER_MASK  = POWER_RAM_POWER_S9POWER_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S10POWER_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S10POWER_MASK = POWER_RAM_POWER_S10POWER_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S11POWER_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S11POWER_MASK = POWER_RAM_POWER_S11POWER_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S12POWER_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S12POWER_MASK = POWER_RAM_POWER_S12POWER_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S13POWER_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S13POWER_MASK = POWER_RAM_POWER_S13POWER_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S14POWER_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S14POWER_MASK = POWER_RAM_POWER_S14POWER_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S15POWER_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S15POWER_MASK = POWER_RAM_POWER_S15POWER_Msk,
+#endif
 
-    NRF_POWER_RAMPOWER_S0RETENTION_MASK  = 1UL << NRF_POWER_RAMPOWER_S0RETENTION ,
-    NRF_POWER_RAMPOWER_S1RETENTION_MASK  = 1UL << NRF_POWER_RAMPOWER_S1RETENTION ,
-    NRF_POWER_RAMPOWER_S2RETENTION_MASK  = 1UL << NRF_POWER_RAMPOWER_S2RETENTION ,
-    NRF_POWER_RAMPOWER_S3RETENTION_MASK  = 1UL << NRF_POWER_RAMPOWER_S3RETENTION ,
-    NRF_POWER_RAMPOWER_S4RETENTION_MASK  = 1UL << NRF_POWER_RAMPOWER_S4RETENTION ,
-    NRF_POWER_RAMPOWER_S5RETENTION_MASK  = 1UL << NRF_POWER_RAMPOWER_S5RETENTION ,
-    NRF_POWER_RAMPOWER_S7RETENTION_MASK  = 1UL << NRF_POWER_RAMPOWER_S7RETENTION ,
-    NRF_POWER_RAMPOWER_S8RETENTION_MASK  = 1UL << NRF_POWER_RAMPOWER_S8RETENTION ,
-    NRF_POWER_RAMPOWER_S9RETENTION_MASK  = 1UL << NRF_POWER_RAMPOWER_S9RETENTION ,
-    NRF_POWER_RAMPOWER_S10RETENTION_MASK = 1UL << NRF_POWER_RAMPOWER_S10RETENTION,
-    NRF_POWER_RAMPOWER_S11RETENTION_MASK = 1UL << NRF_POWER_RAMPOWER_S11RETENTION,
-    NRF_POWER_RAMPOWER_S12RETENTION_MASK = 1UL << NRF_POWER_RAMPOWER_S12RETENTION,
-    NRF_POWER_RAMPOWER_S13RETENTION_MASK = 1UL << NRF_POWER_RAMPOWER_S13RETENTION,
-    NRF_POWER_RAMPOWER_S14RETENTION_MASK = 1UL << NRF_POWER_RAMPOWER_S14RETENTION,
-    NRF_POWER_RAMPOWER_S15RETENTION_MASK = (int)(1UL << NRF_POWER_RAMPOWER_S15RETENTION),
+    NRF_POWER_RAMPOWER_S0RETENTION_MASK  = POWER_RAM_POWER_S0RETENTION_Msk,
+    NRF_POWER_RAMPOWER_S1RETENTION_MASK  = POWER_RAM_POWER_S1RETENTION_Msk,
+#if defined(POWER_RAM_POWER_S2RETENTION_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S2RETENTION_MASK  = POWER_RAM_POWER_S2RETENTION_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S3RETENTION_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S3RETENTION_MASK  = POWER_RAM_POWER_S3RETENTION_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S4RETENTION_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S4RETENTION_MASK  = POWER_RAM_POWER_S4RETENTION_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S5RETENTION_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S5RETENTION_MASK  = POWER_RAM_POWER_S5RETENTION_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S6RETENTION_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S6RETENTION_MASK  = POWER_RAM_POWER_S6RETENTION_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S7RETENTION_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S7RETENTION_MASK  = POWER_RAM_POWER_S7RETENTION_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S8RETENTION_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S8RETENTION_MASK  = POWER_RAM_POWER_S8RETENTION_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S9RETENTION_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S9RETENTION_MASK  = POWER_RAM_POWER_S9RETENTION_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S10RETENTION_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S10RETENTION_MASK = POWER_RAM_POWER_S10RETENTION_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S11RETENTION_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S11RETENTION_MASK = POWER_RAM_POWER_S11RETENTION_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S12RETENTION_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S12RETENTION_MASK = POWER_RAM_POWER_S12RETENTION_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S13RETENTION_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S13RETENTION_MASK = POWER_RAM_POWER_S13RETENTION_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S14RETENTION_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S14RETENTION_MASK = POWER_RAM_POWER_S14RETENTION_Msk,
+#endif
+#if defined(POWER_RAM_POWER_S15RETENTION_Msk) || defined(__NRFX_DOXYGEN__)
+    NRF_POWER_RAMPOWER_S15RETENTION_MASK = POWER_RAM_POWER_S15RETENTION_Msk,
+#endif
 } nrf_power_rampower_mask_t;
 #endif // defined(POWER_RAM_POWER_S0POWER_Msk) || defined(__NRFX_DOXYGEN__)
 

hal/nrf_vmc.h
@@ -53,6 +53,12 @@ typedef enum
 #endif
 } nrf_vmc_power_t;
 
+/** @brief Position of power configuration bits for RAM section 0. */
+#define NRF_VMC_POWER_S0_POS VMC_RAM_POWER_S0POWER_Pos
+
+/** @brief Position of retention configuration bits for RAM section 0. */
+#define NRF_VMC_RETENTION_S0_POS VMC_RAM_POWER_S0RETENTION_Pos
+
 #if defined(__GNUC__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpedantic"

helpers/nrfx_ram_ctrl.h
@@ -63,6 +63,7 @@ __STATIC_INLINE void nrfx_ram_ctrl_section_power_mask_enable_set(uint8_t  block_
     nrf_memconf_ramblock_control_mask_enable_set(NRF_MEMCONF, block_idx, section_mask, enable);
 
 #elif defined(NRF_VMC)
+    section_mask <<= NRF_VMC_POWER_S0_POS;
     if (enable)
     {
         nrf_vmc_ram_block_power_set(NRF_VMC, block_idx, (nrf_vmc_power_t)section_mask);
@@ -73,7 +74,7 @@ __STATIC_INLINE void nrfx_ram_ctrl_section_power_mask_enable_set(uint8_t  block_
     }
 
 #elif defined(POWER_PRESENT)
-    section_mask <<= POWER_RAM_POWER_S0POWER_Pos;
+    section_mask <<= NRF_POWER_RAMPOWER_S0POWER_POS;
     if (enable)
     {
         nrf_power_rampower_mask_on(NRF_POWER, block_idx, section_mask);
@@ -106,7 +107,7 @@ __STATIC_INLINE void nrfx_ram_ctrl_section_retention_mask_enable_set(uint8_t  bl
 #endif
 
 #elif defined(NRF_VMC)
-    section_mask <<= VMC_RAM_POWER_S0RETENTION_Pos;
+    section_mask <<= NRF_VMC_RETENTION_S0_POS;
     if (enable)
     {
         nrf_vmc_ram_block_retention_set(NRF_VMC, block_idx, (nrf_vmc_retention_t)section_mask);
@@ -117,7 +118,7 @@ __STATIC_INLINE void nrfx_ram_ctrl_section_retention_mask_enable_set(uint8_t  bl
     }
 
 #elif defined(POWER_PRESENT)
-    section_mask <<= POWER_RAM_POWER_S0RETENTION_Pos;
+    section_mask <<= NRF_POWER_RAMPOWER_S0RETENTION_POS;
     if (enable)
     {
         nrf_power_rampower_mask_on(NRF_POWER, block_idx, section_mask);

commit c7127c4ce3d5ca216fc1663e646d66d639f0354d

NRFX-NONE: nrf_power.h Strict C compliance warning waive
Enumerate values need to fit in the range of int if we are to be
strictly standad compliant.
But for this emum, its highest value is generated from a bitmask
of the 31st bit, which, as a possitive number is out the int range.

This can cause warnings for compilers which are run in a pedantic
enough mode. GCC is one of this, producing a warning like:
"warning: ISO C restricts enumerator values to range of ‘int’"
when run with "-Wpedantic"
Let's disable these warnings for these enums definitions
when building for GCC.
Apart from this warning, GCC seems to properly handle this case.

Note that as this definitions are in headers, the issue is triggered
when a user tries to compile their code with the corresponding warnings
enabled.

The issue was introduced in
ae3e79a0ae38a8f81ed78dd599095b35e0924fd7
Before, the definition of NRF_POWER_RAMPOWER_S15RETENTION_MASK had
the constant cast to (int) to avoid the warning.

Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

hal/nrf_power.h
@@ -492,6 +492,10 @@ typedef enum
 /** @brief Position of retention configuration bits for RAM section 0. */
 #define NRF_POWER_RAMPOWER_S0RETENTION_POS POWER_RAM_POWER_S0RETENTION_Pos
 
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
 /**
  * @brief Bit masks for RAMPOWER register
  *
@@ -589,6 +593,9 @@ typedef enum
     NRF_POWER_RAMPOWER_S15RETENTION_MASK = POWER_RAM_POWER_S15RETENTION_Msk,
 #endif
 } nrf_power_rampower_mask_t;
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
 #endif // defined(POWER_RAM_POWER_S0POWER_Msk) || defined(__NRFX_DOXYGEN__)
 
 #if NRF_POWER_HAS_VREG_CONFIG